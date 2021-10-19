dfd38c35e6f5f399a98652b410bf6ba9e56b3399
Amir Sadri <amirsadriofficial@gmail.com>
2021-10-19 05:38:33 +0330

    Update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Switch, Router} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+      </BrowserRouter>
     </div>
   );
 }
