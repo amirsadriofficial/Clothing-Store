990c1a889c425c93e0efbc38bfb465795da1438f
Amir Sadri <amirsadriofficial@gmail.com>
2021-10-21 06:53:50 +0330

    Update

--- src/container/Layout.jsx
+++ src/container/Layout.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import Header from './Header';
 import Footer from './Footer';
 
-const Layout = () => {
-    <Header/>;
-    <div>Layout</div>;
-    <Footer/>;
+const Layout = (Children) => {
+    return(
+        <div>Layout</div>
+    )
 }
 
 export default Layout;
