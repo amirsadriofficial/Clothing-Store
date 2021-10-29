e7a87742cd837d75eaa5710ec5a1c0a40565aab1
Amir Sadri <amirsadriofficial@gmail.com>
2021-10-29 15:27:44 +0330

    Update

--- src/container/Layout.jsx
+++ src/container/Layout.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
 
-const Layout = () => {
+const Layout = ({ Children }) => {
   return (
     <>
       <Header />
