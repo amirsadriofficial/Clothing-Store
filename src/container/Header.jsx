2360ce330624dd3c113e352e68397fb04c5347bc
Amir Sadri <amirsadriofficial@gmail.com>
2021-11-03 19:26:05 +0330

    Update

--- src/container/Header.jsx
+++ src/container/Header.jsx
@@ -3,27 +3,23 @@ import React from 'react'
 const Header = () => {
   return (
     // Header desktop
-    <div className="container-menu-desktop">
+    <div className="container">
       {/* Topbar */}
       <div className="top-bar">
-        <div className="content-topbar flex-sb-m h-full container">
-          <div className="left-top-bar">
-            Free shipping for standard order over $100
-          </div>
-          <div className="right-top-bar flex-w h-full">
-            <a href="#" className="flex-c-m trans-04 p-lr-25">
-              Help & FAQs
-            </a>
-            <a href="#" className="flex-c-m trans-04 p-lr-25">
-              My Account
-            </a>
-            <a href="#" className="flex-c-m trans-04 p-lr-25">
-              EN
-            </a>
-            <a href="#" className="flex-c-m trans-04 p-lr-25">
-              USD
-            </a>
-          </div>
+        <div className="left">Free shipping for standard order over $100</div>
+        <div className="right">
+          <a href="#" className="flex-c-m trans-04 p-lr-25">
+            Help & FAQs
+          </a>
+          <a href="#" className="flex-c-m trans-04 p-lr-25">
+            My Account
+          </a>
+          <a href="#" className="flex-c-m trans-04 p-lr-25">
+            EN
+          </a>
+          <a href="#" className="flex-c-m trans-04 p-lr-25">
+            USD
+          </a>
         </div>
       </div>
     </div>
