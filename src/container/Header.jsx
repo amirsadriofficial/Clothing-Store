ad15d4caa86d6700ef32e8fa4e248edfcd5504ad
Amir Sadri <amirsadriofficial@gmail.com>
2021-11-04 01:14:54 +0330

    Update

--- src/container/Header.jsx
+++ src/container/Header.jsx
@@ -2,7 +2,33 @@ import React from 'react'
 
 const Header = () => {
   return (
-
+    // Start Header Desktop
+    <div className="container-menu-desktop">
+      {/* Start Topbar */}
+      <div className="top-bar">
+        <div className="content-topbar flex-sb-m h-full container">
+          <div className="left-top-bar">
+            Free shipping for standard order over $100
+          </div>
+          <div className="right-top-bar flex-w h-full">
+            <a href="#/" className="flex-c-m trans-04 p-lr-25">
+              Help & FAQs
+            </a>
+            <a href="#/" className="flex-c-m trans-04 p-lr-25">
+              My Account
+            </a>
+            <a href="#/" className="flex-c-m trans-04 p-lr-25">
+              EN
+            </a>
+            <a href="#/" className="flex-c-m trans-04 p-lr-25">
+              USD
+            </a>
+          </div>
+        </div>
+      </div>
+      {/* End Topbar */}
+    </div>
+    // End Header Desktop
   )
 }
 
