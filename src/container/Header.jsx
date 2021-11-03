b9b8bedfcc14e4e3745c0304e59ff37354de45f8
Amir Sadri <amirsadriofficial@gmail.com>
2021-11-03 22:19:49 +0330

    Update

--- src/container/Header.jsx
+++ src/container/Header.jsx
@@ -8,6 +8,24 @@ const Header = () => {
         Free shopping for standard order over $100
       </div>
     </div>
+    <nav>
+      <div className="container">
+          <div className="nav-wrapper">
+              <a href="#/" className="left brand-logo">Logo</a>
+              <ul className="right hide-on-med-and-down">
+                  <li>
+                      <a href="#">صفحه اصلی</a>
+                  </li>
+                  <li className="active">
+                      <a href="#">درباره ما</a>
+                  </li>
+                  <li>
+                      <a href="#">تماس با ما</a>
+                  </li>
+              </ul>
+          </div>
+      </div>
+    </nav>
   )
 }
 
