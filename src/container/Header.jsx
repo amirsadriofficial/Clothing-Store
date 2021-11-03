c51de25e778c4da6276ace0d780e5ea8563832a6
Amir Sadri <amirsadriofficial@gmail.com>
2021-11-03 22:24:36 +0330

    Update

--- src/container/Header.jsx
+++ src/container/Header.jsx
@@ -1,32 +1,7 @@
 import React from 'react'
 
-const Header = () => {
-  return (
-    // Header Desktop
-    <div className="black">
-      <div className="right-align">
-        Free shopping for standard order over $100
-      </div>
-    </div>
-    <nav>
-      <div className="container">
-          <div className="nav-wrapper">
-              <a href="#/" className="left brand-logo">Logo</a>
-              <ul className="right hide-on-med-and-down">
-                  <li>
-                      <a href="#">صفحه اصلی</a>
-                  </li>
-                  <li className="active">
-                      <a href="#">درباره ما</a>
-                  </li>
-                  <li>
-                      <a href="#">تماس با ما</a>
-                  </li>
-              </ul>
-          </div>
-      </div>
-    </nav>
-  )
+const Footer = () => {
+  return <div> Footer </div>
 }
 
-export default Header
+export default Footer
