4946320fdaacf03884c03ef6273c04f07092992b
Amir Sadri <amirsadriofficial@gmail.com>
2021-11-03 19:21:14 +0330

    Update

--- src/container/Header.jsx
+++ src/container/Header.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
 
 const Header = () => {
-  return <div> Header </div>
+  return (
+    // Header desktop
+    <div className="container-menu-desktop">
+      {/* Topbar */}
+      <div className="top-bar">
+        <div className="content-topbar flex-sb-m h-full container">
+          <div className="left-top-bar">
+            Free shipping for standard order over $100
+          </div>
+          <div className="right-top-bar flex-w h-full">
+            <a href="#" className="flex-c-m trans-04 p-lr-25">
+              Help & FAQs
+            </a>
+            <a href="#" className="flex-c-m trans-04 p-lr-25">
+              My Account
+            </a>
+            <a href="#" className="flex-c-m trans-04 p-lr-25">
+              EN
+            </a>
+            <a href="#" className="flex-c-m trans-04 p-lr-25">
+              USD
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Header
