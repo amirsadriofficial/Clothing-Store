ee88b01f9da4941e25962efeda81e19b8e10afa8
Amir Sadri <amirsadriofficial@gmail.com>
2021-11-04 21:22:54 +0330

    Update

--- src/container/Header.jsx
+++ src/container/Header.jsx
@@ -3,14 +3,16 @@ import React from 'react'
 const Header = () => {
   return (
     <div id="top-bar" className="black">
-      <div className="left-align">
-        Free shipping for standard order over $100
-      </div>
-      <div className="right-align">
-        <a href="#/">Help & FAQs</a>
-        <a href="#/">My Account</a>
-        <a href="#/">EN</a>
-        <a href="#/">USD</a>
+      <div className="container">
+        <div className="left-align">
+          Free shipping for standard order over $100
+        </div>
+        <div className="right-align">
+          <a href="#/">Help & FAQs</a>
+          <a href="#/">My Account</a>
+          <a href="#/">EN</a>
+          <a href="#/">USD</a>
+        </div>
       </div>
     </div>
   )
