cca1e57895402a6f2db4bde146b3e55466fefdbe
Amir Sadri <amirsadriofficial@gmail.com>
2021-11-06 23:25:48 +0330

    Update

--- src/container/Footer.jsx
+++ src/container/Footer.jsx
@@ -9,28 +9,20 @@ const Footer = () => {
             <h4 className="stext-301 cl0 p-b-30">Categories</h4>
 
             <ul>
-              <li className="p-b-10">
-                <a href="#/" className="stext-107 cl7 hov-cl1 trans-04">
-                  Women
-                </a>
+              <li>
+                <a href="#/">Women</a>
               </li>
 
-              <li className="p-b-10">
-                <a href="#/" className="stext-107 cl7 hov-cl1 trans-04">
-                  Men
-                </a>
+              <li>
+                <a href="#/">Men</a>
               </li>
 
-              <li className="p-b-10">
-                <a href="#/" className="stext-107 cl7 hov-cl1 trans-04">
-                  Shoes
-                </a>
+              <li>
+                <a href="#/">Shoes</a>
               </li>
 
-              <li className="p-b-10">
-                <a href="#/" className="stext-107 cl7 hov-cl1 trans-04">
-                  Watches
-                </a>
+              <li>
+                <a href="#/">Watches</a>
               </li>
             </ul>
           </div>
