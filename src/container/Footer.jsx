18be9c4ee5725a492e6d5cea9d8d66f41ed82417
Amir Sadri <amirsadriofficial@gmail.com>
2021-11-06 23:34:17 +0330

    Update

--- src/container/Footer.jsx
+++ src/container/Footer.jsx
@@ -11,36 +11,29 @@ const Footer = () => {
               <li>
                 <a href="#/">Women</a>
               </li>
-
               <li>
                 <a href="#/">Men</a>
               </li>
-
               <li>
                 <a href="#/">Shoes</a>
               </li>
-
               <li>
                 <a href="#/">Watches</a>
               </li>
             </ul>
           </div>
-
           <div className="col-sm-6 col-lg-3 p-b-50">
             <h4>Help</h4>
             <ul className="reset-space">
               <li>
                 <a href="#/">Track Order</a>
               </li>
-
               <li>
                 <a href="#/">Returns</a>
               </li>
-
               <li>
                 <a href="#/">Shipping</a>
               </li>
-
               <li>
                 <a href="#/">FAQs</a>
               </li>
