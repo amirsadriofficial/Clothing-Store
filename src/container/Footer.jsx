320f53138d39cf863395f7211aabd615a0439ea5
Amir Sadri <amirsadriofficial@gmail.com>
2021-11-07 01:21:06 +0330

    Update

--- src/container/Footer.jsx
+++ src/container/Footer.jsx
@@ -6,23 +6,6 @@ const Footer = () => {
       <div className="container">
         <div className="row">
           <div className="col-sm-6 col-lg-3 p-b-50">
-            <h4>Categories</h4>
-            <ul className="reset-space">
-              <li>
-                <a href="#/">Women</a>
-              </li>
-              <li>
-                <a href="#/">Men</a>
-              </li>
-              <li>
-                <a href="#/">Shoes</a>
-              </li>
-              <li>
-                <a href="#/">Watches</a>
-              </li>
-            </ul>
-          </div>
-          <div className="col-sm-6 col-lg-3 p-b-50">
             <h4>Help</h4>
             <ul className="reset-space">
               <li>
@@ -36,6 +19,41 @@ const Footer = () => {
               </li>
               <li>
                 <a href="#/">FAQs</a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-sm-6 col-lg-3 p-b-50">
+            <h4 className="stext-301 cl0 p-b-30">GET IN TOUCH</h4>
+            <p className="stext-107 cl7 size-201">
+              Any questions? Let us know in store at 8th floor, 379 Hudson St,
+              New York, NY 10018 or call us on (+1) 96 716 6879
+            </p>
+            <div className="p-t-27">
+              <a href="#/" className="fs-18 cl7 hov-cl1 trans-04 m-r-16">
+                <i className="fa fa-facebook" />
+              </a>
+              <a href="#/" className="fs-18 cl7 hov-cl1 trans-04 m-r-16">
+                <i className="fa fa-instagram" />
+              </a>
+              <a href="#/" className="fs-18 cl7 hov-cl1 trans-04 m-r-16">
+                <i className="fa fa-pinterest-p" />
+              </a>
+            </div>
+          </div>
+          <div className="col-sm-6 col-lg-3 p-b-50">
+            <h4>Categories</h4>
+            <ul className="reset-space">
+              <li>
+                <a href="#/">Women</a>
+              </li>
+              <li>
+                <a href="#/">Men</a>
+              </li>
+              <li>
+                <a href="#/">Shoes</a>
+              </li>
+              <li>
+                <a href="#/">Watches</a>
               </li>
             </ul>
           </div>
