40b6ec1d3d69e152dc7282ea725d7e04b5587911
Amir Sadri <amirsadriofficial@gmail.com>
2021-11-06 23:06:49 +0330

    Update

--- src/container/Footer.jsx
+++ src/container/Footer.jsx
@@ -1,7 +1,73 @@
 import React from 'react'
 
 const Footer = () => {
-  return <div> Footer </div>
+  return (
+    <div id="footer">
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6 col-lg-3 p-b-50">
+            <h4 className="stext-301 cl0 p-b-30">Categories</h4>
+
+            <ul>
+              <li className="p-b-10">
+                <a href="#" className="stext-107 cl7 hov-cl1 trans-04">
+                  Women
+                </a>
+              </li>
+
+              <li className="p-b-10">
+                <a href="#" className="stext-107 cl7 hov-cl1 trans-04">
+                  Men
+                </a>
+              </li>
+
+              <li className="p-b-10">
+                <a href="#" className="stext-107 cl7 hov-cl1 trans-04">
+                  Shoes
+                </a>
+              </li>
+
+              <li className="p-b-10">
+                <a href="#" className="stext-107 cl7 hov-cl1 trans-04">
+                  Watches
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="col-sm-6 col-lg-3 p-b-50">
+            <h4 className="stext-301 cl0 p-b-30">Help</h4>
+
+            <ul>
+              <li className="p-b-10">
+                <a href="#" className="stext-107 cl7 hov-cl1 trans-04">
+                  Track Order
+                </a>
+              </li>
+
+              <li className="p-b-10">
+                <a href="#" className="stext-107 cl7 hov-cl1 trans-04">
+                  Returns
+                </a>
+              </li>
+
+              <li className="p-b-10">
+                <a href="#" className="stext-107 cl7 hov-cl1 trans-04">
+                  Shipping
+                </a>
+              </li>
+
+              <li className="p-b-10">
+                <a href="#" className="stext-107 cl7 hov-cl1 trans-04">
+                  FAQs
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Footer
