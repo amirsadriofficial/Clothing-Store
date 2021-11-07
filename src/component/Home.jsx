9a82d553ef8a314fb7e8bd89d900e66001bdaea5
Amir Sadri <amirsadriofficial@gmail.com>
2021-11-08 00:23:00 +0330

    Update

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import Slider from 'react-slick'
-// import '~slick-carousel/slick/slick.css'
-// import '~slick-carousel/slick/slick-theme.css'
-import slideOne from '../images/root/slide-01.jpg'
-import slideTwo from '../images/root/slide-02.jpg'
-import slideThree from '../images/root/slide-03.jpg'
+import PropTypes from 'prop-types'
 
 const Home = () => {
   const settings = {
@@ -13,39 +9,28 @@ const Home = () => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    // autoplay: true,
+    // slickPrev: true,
+    // slickNext: true,
   }
   return (
     <>
-      <div>
-        <img src={slideOne} alt="" className="slide-landing-page" />
-        <img src={slideTwo} alt="" className="slide-landing-page" />
-        <img src={slideThree} alt="" className="slide-landing-page" />
-      </div>
-      <div>
-        <h2> Single Item</h2>
-        <Slider {...settings}>
-          <div>
-            <h3>1</h3>
-          </div>
-          <div>
-            <h3>2</h3>
-          </div>
-          <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-          <div>
-            <h3>5</h3>
-          </div>
-          <div>
-            <h3>6</h3>
-          </div>
-        </Slider>
-      </div>
+      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+      <Slider {...settings}>
+        <div>
+          <div id="slideOne" className="slide-landing-page" />
+        </div>
+        <div>
+          <div id="slideTwo" className="slide-landing-page" />
+        </div>
+        <div>
+          <div id="slideThree" className="slide-landing-page" />
+        </div>
+      </Slider>
     </>
   )
 }
+
+Home.propTypes = { settings: PropTypes.node.isRequired }
 
 export default Home
