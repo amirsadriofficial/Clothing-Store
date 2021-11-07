44e876a820cccd2c6c3878c6ae02b8dd5f8c3096
Amir Sadri <amirsadriofficial@gmail.com>
2021-11-07 19:12:52 +0330

    Update

---
+++ src/component/Shop.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../container/Layout'
+
+const Shop = () => {
+  return (
+    <Layout>
+      <div>Shop</div>
+    </Layout>
+  )
+}
+
+export default Shop
