2000d3e8a0e334ede6246052ae189337725788d8
Amir Sadri <amirsadriofficial@gmail.com>
2021-11-09 00:22:37 +0330

    Update

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,36 +1,7 @@
 import React from 'react'
-import Slider from 'react-slick'
-import PropTypes from 'prop-types'
 
 const Home = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    // autoplay: true,
-    // slickPrev: true,
-    // slickNext: true,
-  }
-  return (
-    <>
-      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-      <Slider {...settings}>
-        <div id="slideOne" className="slide-landing-page">
-          <div>A</div>
-        </div>
-        <div id="slideTwo" className="slide-landing-page">
-          <div>B</div>
-        </div>
-        <div id="slideThree" className="slide-landing-page">
-          <div>C</div>
-        </div>
-      </Slider>
-    </>
-  )
+  return <></>
 }
-
-Home.propTypes = { settings: PropTypes.node.isRequired }
 
 export default Home
