2e19836be1764016ad0ad8d77c047eac3e92600b
Amir Sadri <amirsadriofficial@gmail.com>
2021-11-10 01:26:45 +0330

    Update

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,33 +1,11 @@
 import React from 'react'
-import Slider from 'react-slick'
-import PropTypes from 'prop-types'
 import bannerOne from '../images/banner/banner-01.jpg'
 import bannerTwo from '../images/banner/banner-02.jpg'
 import bannerThree from '../images/banner/banner-03.jpg'
 
 const Home = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-  }
   return (
     <>
-      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-      <Slider {...settings}>
-        <div id="slideOne" className="slide-landing-page">
-          <div>A</div>
-        </div>
-        <div id="slideTwo" className="slide-landing-page">
-          <div>B</div>
-        </div>
-        <div id="slideThree" className="slide-landing-page">
-          <div>C</div>
-        </div>
-      </Slider>
       <section className="container my-5 d-flex flex-row justify-content-center align-item-center">
         <div className="d-flex flex-row p-4">
           <div className="d-flex flex-column justify-content-center align-item-center absoluteOne">
@@ -60,6 +38,6 @@ const Home = () => {
   )
 }
 
-Home.propTypes = { settings: PropTypes.node.isRequired }
+// Home.propTypes = { settings: PropTypes.node.isRequired }
 
 export default Home
--- src/component/Home/Slider.jsx
+++ src/component/Home/Slider.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import Slider from 'react-slick'
+import PropTypes from 'prop-types'
 
 const Slider = () => {
-  return <div>Slider</div>
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+  }
+  return (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <Slider {...settings}>
+      <div id="slideOne" className="slide-landing-page">
+        <div>A</div>
+      </div>
+      <div id="slideTwo" className="slide-landing-page">
+        <div>B</div>
+      </div>
+      <div id="slideThree" className="slide-landing-page">
+        <div>C</div>
+      </div>
+    </Slider>
+  )
 }
+
+Slider.propTypes = { settings: PropTypes.node.isRequired }
 
 export default Slider
