179f72e87f37f1fddbb99d0a4aef21202cd41149
Amir Sadri <amirsadriofficial@gmail.com>
2021-11-10 01:33:14 +0330

    Update

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 // import PropTypes from 'prop-types'
+import SliderComponent from './Slider'
+import Banner from './Banner'
 
 const Home = () => {
   return (
     <>
+      <SliderComponent />
+      <Banner />
       <section className="container">
         <div>
           <h2>Product Overview</h2>
