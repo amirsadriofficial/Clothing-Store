73d62a6e583eb6f3ebdd93a18f18417ea15e27ed
Amir Sadri <amirsadriofficial@gmail.com>
2021-11-10 01:18:36 +0330

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import './App.css'
 import Layout from './container/Layout'
-import Home from './component/Home'
-import Shop from './component/Shop'
-import About from './component/About'
-import Contact from './component/Contact'
-import Cart from './component/Cart'
+import Home from './component/Home/Home'
+import Shop from './component/Shop/Shop'
+import About from './component/About/About'
+import Contact from './component/Contact/Contact'
+import Cart from './component/Cart/Cart'
 
 function App() {
   return (
