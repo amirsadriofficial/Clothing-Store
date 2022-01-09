8f9e833fce8c9efacba8951bf077b6e5861a4b1d
Amir Sadri <amirsadriofficial@gmail.com>
2022-01-09 11:40:10 +0330

    Update

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap/dist/css/bootstrap.css'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import App from './App'
