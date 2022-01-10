e88caa30a6df3b73799fe499fe4899c07a516544
Amir Sadri <amirsadriofficial@gmail.com>
2022-01-10 21:29:14 +0330

    Update

--- src/container/Header/TopBar.jsx
+++ src/container/Header/TopBar.jsx
@@ -1,7 +1,39 @@
 import React from 'react'
+import Box from '@material-ui/core/Box'
+import Container from '@material-ui/core/Container'
+import Grid from '@material-ui/core/Grid'
+// import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(() => ({
+  box: {
+    height: '40px',
+    backgroundColor: '#222',
+  },
+  flex: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  color: {
+    color: '#666',
+  },
+}))
 
 const TopBar = () => {
-  return <div>TopBar</div>
+  const classes = useStyles()
+
+  return (
+    <Box className={classes.box}>
+      <Container className={classes.flex}>
+        <Grid className={classes.color}>
+          Free shipping for standard order over $100
+        </Grid>
+        <Grid className={classes.color}>aaaaaaaa</Grid>
+      </Container>
+    </Box>
+  )
 }
 
 export default TopBar
