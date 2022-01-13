d1791e722cb2b275d8ba0bd852563a1d7bea890c
Amir Sadri <amirsadriofficial@gmail.com>
2022-01-13 17:23:40 +0330

    Update

--- src/container/Header/NavBar.jsx
+++ src/container/Header/NavBar.jsx
@@ -97,14 +97,16 @@ const NavBar = () => {
               <Menu
                 id="menu-appbar"
                 anchorEl={anchorElNav}
+                // elevation={0}
+                getContentAnchorEl={null}
                 anchorOrigin={{
                   vertical: 'bottom',
-                  horizontal: 'left',
+                  horizontal: 'center',
                 }}
                 keepMounted
                 transformOrigin={{
                   vertical: 'top',
-                  horizontal: 'left',
+                  horizontal: 'center',
                 }}
                 open={Boolean(anchorElNav)}
                 onClose={handleCloseNavMenu}
