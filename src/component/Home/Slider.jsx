32d3f1bd96fb7effc64e1bd38f5f4be075e3d7de
Amir Sadri <amirsadriofficial@gmail.com>
2022-01-14 00:53:00 +0330

    Update

--- src/component/Home/Slider.jsx
+++ src/component/Home/Slider.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-
+// import Carousel from 'react-material-ui-carousel'
 // import PropTypes from 'prop-types'
 
 const SliderComponent = () => {
-  return (
-
-  )
+  return <div>Slider</div>
 }
 
 export default SliderComponent
