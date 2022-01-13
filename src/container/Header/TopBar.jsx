49adfbed9358c1d66fd3221f030f563355890b7d
Amir Sadri <amirsadriofficial@gmail.com>
2022-01-13 20:09:19 +0330

    Update

--- src/container/Header/TopBar.jsx
+++ src/container/Header/TopBar.jsx
@@ -3,12 +3,13 @@ import Box from '@material-ui/core/Box'
 import Container from '@material-ui/core/Container'
 import Grid from '@material-ui/core/Grid'
 import Button from '@material-ui/core/Button'
+import Hidden from '@material-ui/core/Hidden'
 import { makeStyles } from '@material-ui/core/styles'
 import clsx from 'clsx'
 
 const useStyles = makeStyles(() => ({
   box: {
-    height: '40px',
+    // height: '40px',
     backgroundColor: '#222',
   },
   flex: {
@@ -31,6 +32,11 @@ const useStyles = makeStyles(() => ({
     paddingLeft: '15px',
     borderLeft: '1px solid rgba(255,255,255,0.3)',
   },
+  borderResponsive: {
+    // paddingRight: '5px',
+    // paddingLeft: '5px',
+    borderLeft: '1px solid rgba(255,255,255,0.3)',
+  },
   aloneBorder: {
     borderRight: '1px solid rgba(255,255,255,0.3)',
   },
@@ -41,33 +47,66 @@ const TopBar = () => {
 
   return (
     <Box className={classes.box}>
-      <Container className={classes.flex}>
-        <Grid className={classes.TextColor}>
-          Free shipping for standard order over $100
-        </Grid>
-        <Grid className={classes.flexGrid}>
-          <Grid className={classes.border}>
-            <Button className={classes.TextColor} href="#">
-              Help & FAQs
-            </Button>
+      <Hidden smDown>
+        <Container className={classes.flex}>
+          <Grid className={classes.TextColor}>
+            Free shipping for standard order over $100
           </Grid>
-          <Grid className={classes.border}>
-            <Button className={classes.TextColor} href="#">
-              My Account
-            </Button>
+          <Grid className={classes.flexGrid}>
+            <Grid className={classes.border}>
+              <Button className={classes.TextColor} href="#">
+                Help & FAQs
+              </Button>
+            </Grid>
+            <Grid className={classes.border}>
+              <Button className={classes.TextColor} href="#">
+                My Account
+              </Button>
+            </Grid>
+            <Grid className={classes.border}>
+              <Button className={classes.TextColor} href="#">
+                EN
+              </Button>
+            </Grid>
+            <Grid className={clsx(classes.border, classes.aloneBorder)}>
+              <Button className={classes.TextColor} href="#">
+                USD
+              </Button>
+            </Grid>
           </Grid>
-          <Grid className={classes.border}>
-            <Button className={classes.TextColor} href="#">
-              EN
-            </Button>
+        </Container>
+      </Hidden>
+      <Hidden mdUp>
+        <Container>
+          <Grid className={classes.TextColor}>
+            Free shipping for standard order over $100
           </Grid>
-          <Grid className={clsx(classes.border, classes.aloneBorder)}>
-            <Button className={classes.TextColor} href="#">
-              USD
-            </Button>
+          <Grid className={classes.flexGrid}>
+            <Grid className={classes.borderResponsive}>
+              <Button className={classes.TextColor} size="small" href="#">
+                Help & FAQs
+              </Button>
+            </Grid>
+            <Grid className={classes.borderResponsive}>
+              <Button className={classes.TextColor} size="small" href="#">
+                My Account
+              </Button>
+            </Grid>
+            <Grid className={classes.borderResponsive}>
+              <Button className={classes.TextColor} size="small" href="#">
+                EN
+              </Button>
+            </Grid>
+            <Grid
+              className={clsx(classes.borderResponsive, classes.aloneBorder)}
+            >
+              <Button className={classes.TextColor} size="small" href="#">
+                USD
+              </Button>
+            </Grid>
           </Grid>
-        </Grid>
-      </Container>
+        </Container>
+      </Hidden>
     </Box>
   )
 }
