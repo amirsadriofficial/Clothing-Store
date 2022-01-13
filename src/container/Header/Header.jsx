2fa233a10e7ac57c4a95eb5908d39f7132f180dd
Amir Sadri <amirsadriofficial@gmail.com>
2022-01-13 23:44:29 +0330

    Update

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import TopBar from './TopBar'
 import NavBar from './NavBar'
-// import Test from './Test'
 
 const Header = () => {
   return (
     <>
       <TopBar />
       <NavBar />
-      {/* <Test /> */}
     </>
   )
 }
