8b2218a438b733f69fd437b77b30adaec689df51
Amir Sadri <amirsadriofficial@gmail.com>
2022-01-15 19:46:24 +0330

    Update

--- src/component/Home/Slider.jsx
+++ src/component/Home/Slider.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Carousel from 'react-material-ui-carousel'
 import SlideOne from '../../images/slide/slide-01.jpg'
+import SlideTwo from '../../images/slide/slide-02.jpg'
+import SlideThree from '../../images/slide/slide-03.jpg'
 
 const SliderComponent = () => {
   return (
