ce6e9baf211f10e2beb3ea6ebaac1ec400543d33
Amir Sadri <amirsadriofficial@gmail.com>
2022-01-15 19:52:28 +0330

    Update

--- src/component/Home/Slider.jsx
+++ src/component/Home/Slider.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import Carousel from 'react-material-ui-carousel'
+import { makeStyles } from '@material-ui/core/styles'
 import SlideOne from '../../images/slide/slide-01.jpg'
 import SlideTwo from '../../images/slide/slide-02.jpg'
 import SlideThree from '../../images/slide/slide-03.jpg'
 
+const useStyles = makeStyles(() => ({
+  carousel: {
+    height: '50vh',
+  },
+}))
+
 const SliderComponent = () => {
+  const classes = useStyles()
+
   return (
-    <Carousel>
+    <Carousel className={classes.carousel}>
       <img src={SlideOne} alt="SlideOne" />
       <img src={SlideTwo} alt="SlideOne" />
       <img src={SlideThree} alt="SlideOne" />
