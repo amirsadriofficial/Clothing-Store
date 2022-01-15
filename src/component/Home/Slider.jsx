9dbd178c5983c8a77337888e8150342ae61013df
Amir Sadri <amirsadriofficial@gmail.com>
2022-01-15 19:39:32 +0330

    Update

--- src/component/Home/Slider.jsx
+++ src/component/Home/Slider.jsx
@@ -10,21 +10,21 @@
 
 import React from 'react'
 import Carousel from 'react-material-ui-carousel'
-import PropTypes from 'prop-types'
-import Paper from '@material-ui/core/Paper'
-import Button from '@material-ui/core/Button'
+// import PropTypes from 'prop-types'
+// import Paper from '@material-ui/core/Paper'
+// import Button from '@material-ui/core/Button'
 
-const SliderComponent = (props) => {
-  const items = [
-    {
-      name: 'Random Name #1',
-      description: 'Probably the most random thing you have ever seen!',
-    },
-    {
-      name: 'Random Name #2',
-      description: 'Hello World!',
-    },
-  ]
+const SliderComponent = () => {
+  // const items = [
+  //   {
+  //     name: 'Random Name #1',
+  //     description: 'Probably the most random thing you have ever seen!',
+  //   },
+  //   {
+  //     name: 'Random Name #2',
+  //     description: 'Hello World!',
+  //   },
+  // ]
 
   return (
     <Carousel>
@@ -35,14 +35,15 @@ const SliderComponent = (props) => {
   )
 }
 
-function Item(props) {
-  return (
-    <Paper>
-      <h2>{props.item.name}</h2>
-      <p>{props.item.description}</p>
+// function Item(props) {
+//   return (
+//     <Paper>
+//       <h2>{props.item.name}</h2>
+//       <p>{props.item.description}</p>
 
-      <Button className="CheckButton">Check it out!</Button>
-    </Paper>
-  )
-}
+//       <Button className="CheckButton">Check it out!</Button>
+//     </Paper>
+//   )
+// }
+
 export default SliderComponent
