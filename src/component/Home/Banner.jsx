3180c4dd94786adb7595ffb3c5bc65f65062850f
Amir Sadri <amirsadriofficial@gmail.com>
2022-01-17 21:41:46 +0330

    Update

--- src/component/Home/Banner.jsx
+++ src/component/Home/Banner.jsx
@@ -1,36 +1,39 @@
 import React from 'react'
+import Container from '@material-ui/core/Container'
 import Grid from '@material-ui/core/Grid'
-// import { makeStyles } from '@material-ui/core/styles'
-// import bannerOne from '../../images/banner/banner-01.jpg'
-// import bannerTwo from '../../images/banner/banner-02.jpg'
-// import bannerThree from '../../images/banner/banner-03.jpg'
+import { makeStyles } from '@material-ui/core/styles'
+import bannerOne from '../../images/banner/banner-01.jpg'
+import bannerTwo from '../../images/banner/banner-02.jpg'
+import bannerThree from '../../images/banner/banner-03.jpg'
 
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     flexGrow: 1,
-//   },
-//   paper: {
-//     padding: theme.spacing(2),
-//     textAlign: 'center',
-//     color: theme.palette.text.secondary,
-//   },
-// }))
+const useStyles = makeStyles(() => ({
+  containerSpacing: {
+    marginTop: '80px',
+    marginBottom: '80px',
+  },
+  img: {
+    width: '100%',
+    height: '100%',
+  },
+}))
 
 const Banner = () => {
-  // const classes = useStyles()
+  const classes = useStyles()
 
   return (
-    <Grid container spacing={3}>
-      <Grid item xs={3}>
-        5
+    <Container className={classes.containerSpacing}>
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={6} lg={4}>
+          <img src={bannerOne} alt="bannerOne" className={classes.img} />
+        </Grid>
+        <Grid item xs={12} md={6} lg={4}>
+          <img src={bannerTwo} alt="bannerTwo" className={classes.img} />
+        </Grid>
+        <Grid item xs={12} md={6} lg={4}>
+          <img src={bannerThree} alt="bannerThree" className={classes.img} />
+        </Grid>
       </Grid>
-      <Grid item xs={3}>
-        6
-      </Grid>
-      <Grid item xs={3}>
-        7
-      </Grid>
-    </Grid>
+    </Container>
   )
 }
 
