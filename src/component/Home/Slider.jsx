149e7743b7daad87e581b93860fa5309cce31bdd
Amir Sadri <amirsadriofficial@gmail.com>
2022-01-17 17:26:54 +0330

    Update

--- src/component/Home/Slider.jsx
+++ src/component/Home/Slider.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import Carousel from 'react-material-ui-carousel'
 import { makeStyles } from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button'
-import clsx from 'clsx'
 import Grid from '@material-ui/core/Grid'
+import Hidden from '@material-ui/core/Hidden'
+import clsx from 'clsx'
 import SlideOne from '../../images/slide/slide-01.jpg'
 import SlideTwo from '../../images/slide/slide-02.jpg'
 import SlideThree from '../../images/slide/slide-03.jpg'
@@ -18,10 +19,33 @@ const useStyles = makeStyles(() => ({
     bottom: '350px',
     left: '200px',
   },
+  descriptionPositionll: {
+    position: 'absolute',
+    bottom: '50px',
+    left: '20px',
+  },
+  descriptionPositionMd: {
+    position: 'absolute',
+    bottom: '100px',
+    left: '80px',
+  },
+  descriptionPositionSm: {
+    position: 'absolute',
+    bottom: '70px',
+    left: '35px',
+  },
+  descriptionFontSm: {
+    fontSize: '16px',
+  },
   buttonStyle: {
     backgroundColor: '#717fe0',
     borderRadius: '25px',
     marginTop: '35px',
+    marginBottom: '1px',
+  },
+  buttonStyleSm: {
+    backgroundColor: '#717fe0',
+    borderRadius: '25px',
     marginBottom: '1px',
   },
 }))
@@ -33,18 +57,72 @@ const SliderComponent = () => {
     <Carousel autoPlay={false}>
       <div>
         <img src={SlideOne} alt="SlideOne" className={classes.responsive} />
-        <Grid className={classes.descriptionPosition}>
-          <h2>Woman Collection 2022</h2>
-          <p>NEW SEASON</p>
-          <Button
-            className={clsx(classes.buttonStyle, classes.buttonResponsiveStyle)}
-            size="large"
-            variant="contained"
-            color="primary"
-          >
-            Shop Now
-          </Button>
-        </Grid>
+        <Hidden mdDown>
+          <Grid className={classes.descriptionPosition}>
+            <h2>Woman Collection 2022</h2>
+            <p>NEW SEASON</p>
+            <Button
+              className={clsx(
+                classes.buttonStyle,
+                classes.buttonResponsiveStyle
+              )}
+              size="large"
+              variant="contained"
+              color="primary"
+            >
+              Shop Now
+            </Button>
+          </Grid>
+        </Hidden>
+        <Hidden lgUp smDown>
+          <Grid className={classes.descriptionPositionll}>
+            {/* <h2>Woman Collection 2022</h2>
+            <p>NEW SEASON</p> */}
+            <Button
+              className={clsx(
+                classes.buttonStyle,
+                classes.buttonResponsiveStyle
+              )}
+              variant="contained"
+              color="primary"
+            >
+              Shop Now
+            </Button>
+          </Grid>
+        </Hidden>
+        <Hidden mdUp xsDown>
+          <Grid className={classes.descriptionPositionMd}>
+            <h2>Woman Collection 2022</h2>
+            <p>NEW SEASON</p>
+            <Button
+              className={clsx(
+                classes.buttonStyle,
+                classes.buttonResponsiveStyle
+              )}
+              variant="contained"
+              color="primary"
+            >
+              Shop Now
+            </Button>
+          </Grid>
+        </Hidden>
+        <Hidden smUp>
+          <Grid className={classes.descriptionPositionSm}>
+            <h2 className={classes.descriptionFontSm}>Woman Collection 2022</h2>
+            <p className={classes.descriptionFontSm}>NEW SEASON</p>
+            <Button
+              className={clsx(
+                classes.buttonStyleSm,
+                classes.buttonResponsiveStyle
+              )}
+              size="small"
+              variant="contained"
+              color="primary"
+            >
+              Shop Now
+            </Button>
+          </Grid>
+        </Hidden>
       </div>
       <div>
         <img src={SlideTwo} alt="SlideTwo" className={classes.responsive} />
