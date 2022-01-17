8d3452db766d416b07d76080d375a021a91a1623
Amir Sadri <amirsadriofficial@gmail.com>
2022-01-17 18:43:23 +0330

    Update

--- src/component/Home/slider/Slider.jsx
+++ src/component/Home/slider/Slider.jsx
@@ -1,62 +1,68 @@
 import React from 'react'
 import Carousel from 'react-material-ui-carousel'
-import { makeStyles } from '@material-ui/core/styles'
-import Button from '@material-ui/core/Button'
-import Grid from '@material-ui/core/Grid'
-import Hidden from '@material-ui/core/Hidden'
-import SlideOne from '../../../images/slide/slide-01.jpg'
-import SlideTwo from '../../../images/slide/slide-02.jpg'
-import SlideThree from '../../../images/slide/slide-03.jpg'
+// import { makeStyles } from '@material-ui/core/styles'
+// import Button from '@material-ui/core/Button'
+// import Grid from '@material-ui/core/Grid'
+// import Hidden from '@material-ui/core/Hidden'
+// import SlideOne from '../../../images/slide/slide-01.jpg'
+// import SlideTwo from '../../../images/slide/slide-02.jpg'
+// import SlideThree from '../../../images/slide/slide-03.jpg'
+import Slide1 from './Slide1'
+import Slide2 from './Slide2'
+import Slide3 from './Slide3'
 
-const useStyles = makeStyles(() => ({
-  responsive: {
-    width: '100%',
-    height: 'auto',
-  },
-  descriptionPosition: {
-    position: 'absolute',
-    bottom: '250px',
-    left: '125px',
-  },
-  descriptionPositionLg: {
-    position: 'absolute',
-    bottom: '200px',
-    left: '100px',
-  },
-  descriptionPositionMd: {
-    position: 'absolute',
-    bottom: '150px',
-    left: '75px',
-  },
-  descriptionPositionSm: {
-    position: 'absolute',
-    bottom: '90px',
-    left: '40px',
-  },
-  descriptionFont: {
-    fontSize: '50px',
-  },
-  descriptionFontLg: {
-    fontSize: '35px',
-  },
-  descriptionFontMd: {
-    fontSize: '25px',
-  },
-  descriptionFontSm: {
-    fontSize: '15px',
-  },
-  buttonStyle: {
-    backgroundColor: '#717fe0',
-    borderRadius: '25px',
-  },
-}))
+// const useStyles = makeStyles(() => ({
+//   responsive: {
+//     width: '100%',
+//     height: 'auto',
+//   },
+//   descriptionPosition: {
+//     position: 'absolute',
+//     bottom: '250px',
+//     left: '125px',
+//   },
+//   descriptionPositionLg: {
+//     position: 'absolute',
+//     bottom: '200px',
+//     left: '100px',
+//   },
+//   descriptionPositionMd: {
+//     position: 'absolute',
+//     bottom: '150px',
+//     left: '75px',
+//   },
+//   descriptionPositionSm: {
+//     position: 'absolute',
+//     bottom: '90px',
+//     left: '40px',
+//   },
+//   descriptionFont: {
+//     fontSize: '50px',
+//   },
+//   descriptionFontLg: {
+//     fontSize: '35px',
+//   },
+//   descriptionFontMd: {
+//     fontSize: '25px',
+//   },
+//   descriptionFontSm: {
+//     fontSize: '15px',
+//   },
+//   buttonStyle: {
+//     backgroundColor: '#717fe0',
+//     borderRadius: '25px',
+//   },
+// }))
 
 const Slider = () => {
-  const classes = useStyles()
+  // const classes = useStyles()
 
   return (
     <Carousel>
-      <div>
+      <Slide1 />
+      <Slide2 />
+      <Slide3 />
+      {/* <div>
         <img src={SlideOne} alt="SlideOne" className={classes.responsive} />
         <Hidden mdDown>
           <Grid className={classes.descriptionPosition}>
@@ -232,7 +238,7 @@ const Slider = () => {
             </Button>
           </Grid>
         </Hidden>
-      </div>
+      </div> */}
     </Carousel>
   )
 }
