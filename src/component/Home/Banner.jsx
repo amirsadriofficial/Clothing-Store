626b1654e1d717bccf1789afb21ac8eac6412615
Amir Sadri <amirsadriofficial@gmail.com>
2022-01-17 21:27:59 +0330

    Update

--- src/component/Home/Banner.jsx
+++ src/component/Home/Banner.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
+import Grid from '@material-ui/core/Grid'
+// import { makeStyles } from '@material-ui/core/styles'
 // import bannerOne from '../../images/banner/banner-01.jpg'
 // import bannerTwo from '../../images/banner/banner-02.jpg'
 // import bannerThree from '../../images/banner/banner-03.jpg'
 
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     flexGrow: 1,
+//   },
+//   paper: {
+//     padding: theme.spacing(2),
+//     textAlign: 'center',
+//     color: theme.palette.text.secondary,
+//   },
+// }))
+
 const Banner = () => {
-  return <div>Banner</div>
+  // const classes = useStyles()
+
+  return (
+    <Grid container spacing={3}>
+      <Grid item xs={3}>
+        5
+      </Grid>
+      <Grid item xs={3}>
+        6
+      </Grid>
+      <Grid item xs={3}>
+        7
+      </Grid>
+    </Grid>
+  )
 }
 
 export default Banner
