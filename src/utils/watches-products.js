5d1f64ce5b2477e247fd4580238733f2d4026b05
Amir Sadri <amirsadriofficial@gmail.com>
2022-01-18 15:45:46 +0330

    Update

--- src/utils/watches-products.js
+++ src/utils/watches-products.js
@@ -1,30 +1,51 @@
+import watchProduct1 from '../images/products/watches/watches-product-01.jpg'
+import watchProduct2 from '../images/products/watches/watches-product-02.jpg'
+import watchProduct3 from '../images/products/watches/watches-product-03.webp'
+import watchProduct4 from '../images/products/watches/watches-product-04.webp'
+import watchProduct5 from '../images/products/watches/watches-product-05.webp'
+import watchProduct6 from '../images/products/watches/watches-product-06.webp'
+
 const WATCH_PRODUCTS = [
   {
     id: 'watch_product_1',
     name: 'Watch Product',
     description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-    // image: watch_product_1,
+    image: watchProduct1,
     price: 175,
   },
   {
     id: 'watch_product_2',
     name: 'Watch Product',
     description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-    // image: watch_product_2,
+    image: watchProduct2,
     price: 275,
   },
   {
     id: 'watch_product_3',
     name: 'Watch Product',
     description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-    // image: dwatch_product_3,
+    image: watchProduct3,
     price: 375,
   },
   {
     id: 'watch_product_4',
     name: 'Watch Product',
     description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-    // image: watch_product_4,
+    image: watchProduct4,
+    price: 475,
+  },
+  {
+    id: 'watch_product_4',
+    name: 'Watch Product',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    image: watchProduct5,
+    price: 475,
+  },
+  {
+    id: 'watch_product_4',
+    name: 'Watch Product',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    image: watchProduct6,
     price: 475,
   },
 ]
