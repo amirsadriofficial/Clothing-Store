6fcf2b9eedda3d4d472cfa955f0c76b57b24d738
Amir Sadri <amirsadriofficial@gmail.com>
2022-01-18 15:52:05 +0330

    Update

--- src/utils/woman-products.js
+++ src/utils/woman-products.js
@@ -1,30 +1,83 @@
+import womenProduct1 from '../images/products/woman/woman-product-01.jpg'
+import womenProduct2 from '../images/products/woman/woman-product-02.jpg'
+import womenProduct3 from '../images/products/woman/woman-product-03.jpg'
+import womenProduct4 from '../images/products/woman/woman-product-04.jpg'
+import womenProduct5 from '../images/products/woman/woman-product-05.jpg'
+import womenProduct6 from '../images/products/woman/woman-product-06.jpg'
+import womenProduct7 from '../images/products/woman/woman-product-07.jpg'
+import womenProduct8 from '../images/products/woman/woman-product-08.jpg'
+import womenProduct9 from '../images/products/woman/woman-product-09.jpg'
+import womenProduct10 from '../images/products/woman/woman-product-010.jpg'
+
 const WOMEN_PRODUCTS = [
   {
     id: 'women_product_1',
     name: 'Women Product',
     description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-    // image: women_product_1,
+    image: women_product_1,
     price: 175,
   },
   {
     id: 'women_product_2',
     name: 'Women Product',
     description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-    // image: women_product_2,
+    image: women_product_2,
     price: 275,
   },
   {
     id: 'women_product_3',
     name: 'Women Product',
     description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-    // image: women_product_3,
+    image: women_product_3,
     price: 375,
   },
   {
     id: 'women_product_4',
     name: 'Women Product',
     description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-    // image: women_product_4,
+    image: women_product_4,
+    price: 475,
+  },
+  {
+    id: 'women_product_4',
+    name: 'Women Product',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    image: women_product_4,
+    price: 475,
+  },
+  {
+    id: 'women_product_4',
+    name: 'Women Product',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    image: women_product_4,
+    price: 475,
+  },
+  {
+    id: 'women_product_4',
+    name: 'Women Product',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    image: women_product_4,
+    price: 475,
+  },
+  {
+    id: 'women_product_4',
+    name: 'Women Product',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    image: women_product_4,
+    price: 475,
+  },
+  {
+    id: 'women_product_4',
+    name: 'Women Product',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    image: women_product_4,
+    price: 475,
+  },
+  {
+    id: 'women_product_4',
+    name: 'Women Product',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    image: women_product_4,
     price: 475,
   },
 ]
