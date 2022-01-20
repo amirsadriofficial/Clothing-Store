4c0f9a23a900260edbd7fb7b82293a7e0a7ac167
Amir Sadri <amirsadriofficial@gmail.com>
2022-01-20 15:34:42 +0330

    Update

--- src/component/Home/banner/Banner2.jsx
+++ src/component/Home/banner/Banner2.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
-import banner from '../../../images/banner/banner2.jpg'
+import Grid from '@material-ui/core/Grid'
+import banner1 from '../../../images/banner/banner2.jpg'
+import banner2 from '../../../images/banner/banner2-2.jpg'
 
 const Banner2 = () => {
   return (
-    <img src={banner} alt="banner" style={{ width: '90%', height: '300px' }} />
+    <Grid
+      container
+      spacing={3}
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+      }}
+    >
+      <Grid item xl={12} sm={7} md={5}>
+        <img
+          src={banner1}
+          alt="banner"
+          style={{ width: '100%', height: 'auto' }}
+        />
+      </Grid>
+      <Grid item xl={12} sm={7} md={6}>
+        <img
+          src={banner2}
+          alt="banner"
+          style={{ width: '100%', height: 'auto' }}
+        />
+      </Grid>
+    </Grid>
   )
 }
 
--- src/component/Home/banner/Banner3.jsx
+++ src/component/Home/banner/Banner3.jsx
@@ -1,7 +1,34 @@
 import React from 'react'
+import Grid from '@material-ui/core/Grid'
+import banner1 from '../../../images/banner/banner2.jpg'
+import banner2 from '../../../images/banner/banner2-2.jpg'
 
-const Banner3 = () => {
-  return <div />
+const Banner2 = () => {
+  return (
+    <Grid
+      container
+      spacing={3}
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+      }}
+    >
+      <Grid item xl={12} sm={7} md={5}>
+        <img
+          src={banner1}
+          alt="banner"
+          style={{ width: '100%', height: 'auto' }}
+        />
+      </Grid>
+      <Grid item xl={12} sm={7} md={6}>
+        <img
+          src={banner2}
+          alt="banner"
+          style={{ width: '100%', height: 'auto' }}
+        />
+      </Grid>
+    </Grid>
+  )
 }
 
-export default Banner3
+export default Banner2
