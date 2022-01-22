0a7e328313f7d3183e2185a5d2edb1da208b4ed3
Amir Sadri <amirsadriofficial@gmail.com>
2022-01-22 14:16:17 +0330

    Update

--- src/component/Contact/Contact.jsx
+++ src/component/Contact/Contact.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import Container from '@material-ui/core/Container'
+import Background from '../../images/background/contact-bg.jpg'
 
 const Contact = () => {
-  return <div>Contact</div>
+  return (
+    <>
+      <img src={Background} alt="" />
+      <h2>Contact</h2>
+      <Container>
+        <div>aaaaaaaaaaa</div>
+      </Container>
+    </>
+  )
 }
 
 export default Contact
