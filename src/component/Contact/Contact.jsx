b1e9c2ec0b1984da985004ef7da742d13f934f69
Amir Sadri <amirsadriofficial@gmail.com>
2022-01-22 17:58:59 +0330

    Update

--- src/component/Contact/Contact.jsx
+++ src/component/Contact/Contact.jsx
@@ -1,22 +1,21 @@
 import React from 'react'
 import Container from '@material-ui/core/Container'
-import Paper from '@material-ui/core/Paper'
 import { makeStyles } from '@material-ui/core/styles'
 import Background from '../../images/background/contact-bg.jpg'
 
 const useStyles = makeStyles({
   background: {
     width: '100%',
-    height: '100%',
-    backgroundImage: `url('${Background}')`,
-    marginBottom: '100px',
+    height: '240px',
+    backgroundImage: `url(${Background})`,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    // marginBottom: '100px,
   },
   title: {
     fontSize: '50px',
     color: '#fff',
-    position: 'absolute',
-    top: '150px',
-    left: '45%',
   },
 })
 
@@ -25,19 +24,25 @@ const Contact = () => {
 
   return (
     <>
-      <Paper className={classes.background}>
-        {/* <img
-          src={Background}
-          alt="Background"
-          style={{ width: '100%', height: '100p' }}
-        /> */}
-        {/* <h2 className={classes.title}>Contact</h2> */}
-      </Paper>
+      <div className={classes.background}>
+        <h2 className={classes.title}>Contact</h2>
+      </div>
       <Container>
-        <div>aaaaaaaaaaa</div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
+          architecto aspernatur officiis veniam voluptates est dolores cum
+          praesentium quo? Iusto facilis odio, doloremque officiis veniam quasi
+          incidunt libero quas illo.
+        </p>
       </Container>
     </>
   )
 }
 
 export default Contact
+
+/* <img
+  src={Background}
+  alt="Background"
+  style={{ width: '', height: '240p' }}
+/> */
