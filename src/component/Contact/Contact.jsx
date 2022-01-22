498bde456ab59cf161a36e0e184e378155298122
Amir Sadri <amirsadriofficial@gmail.com>
2022-01-22 14:33:28 +0330

    Update

--- src/component/Contact/Contact.jsx
+++ src/component/Contact/Contact.jsx
@@ -1,12 +1,36 @@
 import React from 'react'
 import Container from '@material-ui/core/Container'
+import { makeStyles } from '@material-ui/core/styles'
 import Background from '../../images/background/contact-bg.jpg'
 
+const useStyles = makeStyles({
+  background: {
+    width: '100%',
+    height: '100%',
+    backgroundImage: `url('${Background}')`,
+  },
+  title: {
+    fontSize: '50px',
+    color: '#fff',
+    position: 'absolute',
+    top: '150px',
+    left: '45%',
+  },
+})
+
 const Contact = () => {
+  const classes = useStyles()
+
   return (
     <>
-      <img src={Background} alt="" />
-      <h2>Contact</h2>
+      <div className={classes.background}>
+        {/* <img
+          src={Background}
+          alt="Background"
+          style={{ width: '100%', height: '100p' }}
+        /> */}
+        {/* <h2 className={classes.title}>Contact</h2> */}
+      </div>
       <Container>
         <div>aaaaaaaaaaa</div>
       </Container>
