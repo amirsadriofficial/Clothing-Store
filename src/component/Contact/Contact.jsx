6325d0f72abb11d9b5c479cfe938c89a51682708
Amir Sadri <amirsadriofficial@gmail.com>
2022-01-23 20:48:46 +0330

    Update

--- src/component/Contact/Contact.jsx
+++ src/component/Contact/Contact.jsx
@@ -104,7 +104,7 @@ const Contact = () => {
             md={6}
             className={clsx(classes.sectionStyle, classes.firstSectionStyle)}
           >
-            <Grid item sm={9} className={classes.firstSection}>
+            <Grid item xs={9} className={classes.firstSection}>
               <h3 className={classes.titleSection}>Send Us A Message</h3>
               <TextField
                 className={classes.objectSubSection}
@@ -142,28 +142,30 @@ const Contact = () => {
             md={6}
             className={clsx(classes.sectionStyle, classes.secondSection)}
           >
-            <Grid className={classes.secondSubSection}>
-              <LocationOnOutlinedIcon />
-              <Grid className={classes.flexSubSection}>
-                <h4 className={classes.titleSubSection}>Address</h4>
-                <p>
-                  The Amazon Corporate Headquarters address is 410 Terry Ave N,
-                  Seattle 98109, WA.
-                </p>
+            <Grid item xs={9} className={classes.secondSection}>
+              <Grid className={classes.secondSubSection}>
+                <LocationOnOutlinedIcon />
+                <Grid className={classes.flexSubSection}>
+                  <h4 className={classes.titleSubSection}>Address</h4>
+                  <p>
+                    The Amazon Corporate Headquarters address is 410 Terry Ave
+                    N, Seattle 98109, WA.
+                  </p>
+                </Grid>
               </Grid>
-            </Grid>
-            <Grid className={classes.secondSubSection}>
-              <CallOutlinedIcon />
-              <Grid className={classes.flexSubSection}>
-                <h4 className={classes.titleSubSection}>Lets Talk</h4>
-                <p className={classes.textSubSection}>+1 888-280-4331</p>
+              <Grid className={classes.secondSubSection}>
+                <CallOutlinedIcon />
+                <Grid className={classes.flexSubSection}>
+                  <h4 className={classes.titleSubSection}>Lets Talk</h4>
+                  <p className={classes.textSubSection}>+1 888-280-4331</p>
+                </Grid>
               </Grid>
-            </Grid>
-            <Grid className={classes.secondSubSection}>
-              <EmailOutlinedIcon />
-              <Grid className={classes.flexSubSection}>
-                <h4 className={classes.titleSubSection}>Sale Support</h4>
-                <p className={classes.textSubSection}>cis@amazon.com</p>
+              <Grid className={classes.secondSubSection}>
+                <EmailOutlinedIcon />
+                <Grid className={classes.flexSubSection}>
+                  <h4 className={classes.titleSubSection}>Sale Support</h4>
+                  <p className={classes.textSubSection}>cis@amazon.com</p>
+                </Grid>
               </Grid>
             </Grid>
           </Grid>
