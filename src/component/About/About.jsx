d911775330084a28f633dbe98c05a2b16ce2deb6
Amir Sadri <amirsadriofficial@gmail.com>
2022-01-24 13:40:58 +0330

    Update

--- src/component/About/About.jsx
+++ src/component/About/About.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import Container from '@material-ui/core/Container'
 import Grid from '@material-ui/core/Grid'
-import TextField from '@material-ui/core/TextField'
-import Button from '@material-ui/core/Button'
+// import TextField from '@material-ui/core/TextField'
+// import Button from '@material-ui/core/Button'
 import { makeStyles } from '@material-ui/core/styles'
-import clsx from 'clsx'
-import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined'
-import CallOutlinedIcon from '@material-ui/icons/CallOutlined'
-import EmailOutlinedIcon from '@material-ui/icons/EmailOutlined'
+// import clsx from 'clsx'
 import Background from '../../images/background/about-bg.jpg'
 
 const useStyles = makeStyles({
@@ -27,58 +24,6 @@ const useStyles = makeStyles({
     marginBottom: '75px',
     marginTop: '75px',
   },
-  sectionStyle: {
-    border: '1px solid #e6e6e6',
-  },
-  firstSection: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  firstSectionStyle: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  secondSection: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'start',
-  },
-  secondSubSection: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-    alignItems: 'start',
-    paddingLeft: '20px',
-    paddingRight: '20px',
-    marginTop: '30px',
-  },
-  flexSubSection: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'start',
-  },
-  titleSection: {
-    fontSize: '25px',
-    marginBottom: '25px',
-  },
-  objectSubSection: {
-    marginBottom: '20px',
-  },
-  titleSubSection: {
-    marginTop: '0px',
-    fontSize: '18px',
-    marginBottom: '0px',
-    paddingLeft: '10px',
-  },
-  textSubSection: {
-    color: '#717fe0',
-    cursor: 'pointer',
-  },
 })
 
 const About = () => {
@@ -91,73 +36,20 @@ const About = () => {
       </div>
       <Container className={classes.container}>
         <Grid container spacing={1}>
-          <Grid
-            item
-            xs={12}
-            md={6}
-            className={clsx(classes.sectionStyle, classes.firstSectionStyle)}
-          >
-            <Grid item xs={9} className={classes.firstSection}>
-              <h3 className={classes.titleSection}>Send Us A Message</h3>
-              <TextField
-                className={classes.objectSubSection}
-                id="outlined-textarea"
-                label="Your Email Address"
-                placeholder="email@example.com"
-                multiline
-                fullWidth
-                variant="outlined"
-              />
-              <TextField
-                className={classes.objectSubSection}
-                id="outlined-textarea"
-                label="How Can We Help?"
-                placeholder="Write your opinion or problem for us"
-                multiline
-                rows={5}
-                fullWidth
-                variant="outlined"
-              />
-              <Button
-                variant="contained"
-                color="primary"
-                fullWidth
-                className={classes.objectSubSection}
-                style={{ borderRadius: '25px' }}
-              >
-                Submit
-              </Button>
+          <Grid item xs={12}>
+            <Grid item xs={6}>
+              text 1
+            </Grid>
+            <Grid item xs={6}>
+              picture 1
             </Grid>
           </Grid>
-          <Grid
-            item
-            xs={12}
-            md={6}
-            className={clsx(classes.sectionStyle, classes.secondSection)}
-          >
-            <Grid className={classes.secondSubSection}>
-              <LocationOnOutlinedIcon />
-              <Grid className={classes.flexSubSection}>
-                <h4 className={classes.titleSubSection}>Address</h4>
-                <p>
-                  The Amazon Corporate Headquarters address is 410 Terry Ave N,
-                  Seattle 98109, WA.
-                </p>
-              </Grid>
+          <Grid item xs={12}>
+            <Grid item xs={6}>
+              picture 2
             </Grid>
-            <Grid className={classes.secondSubSection}>
-              <CallOutlinedIcon />
-              <Grid className={classes.flexSubSection}>
-                <h4 className={classes.titleSubSection}>Lets Talk</h4>
-                <p className={classes.textSubSection}>+1 888-280-4331</p>
-              </Grid>
-            </Grid>
-            <Grid className={classes.secondSubSection}>
-              <EmailOutlinedIcon />
-              <Grid className={classes.flexSubSection}>
-                <h4 className={classes.titleSubSection}>Sale Support</h4>
-                <p className={classes.textSubSection}>cis@amazon.com</p>
-              </Grid>
+            <Grid item xs={6}>
+              text 2
             </Grid>
           </Grid>
         </Grid>
