03f796e151ff5ebbb79e5def16cc03c2112eb58e
Amir Sadri <amirsadriofficial@gmail.com>
2022-01-27 18:11:29 +0330

    Update

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -8,10 +8,16 @@ import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
 import CardActionArea from '@material-ui/core/CardActionArea'
 // import IconButton from '@material-ui/core/IconButton'
+import CardActions from '@material-ui/core/CardActions'
+import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
-// import SkipPreviousIcon from '@material-ui/icons/SkipPrevious'
-// import PlayArrowIcon from '@material-ui/icons/PlayArrow'
-// import SkipNextIcon from '@material-ui/icons/SkipNext'
+import TextField from '@material-ui/core/TextField'
+import ButtonGroup from '@material-ui/core/ButtonGroup'
+import IconButton from '@material-ui/core/IconButton'
+import AddIcon from '@material-ui/icons/Add'
+import RemoveIcon from '@material-ui/icons/Remove'
+import DeleteIcon from '@material-ui/icons/Delete'
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder'
 import Product from '../../utils/all-products'
 
 const useStyles = makeStyles(() => ({
@@ -24,13 +30,35 @@ const useStyles = makeStyles(() => ({
     height: '100%',
   },
   media: {
-    width: '100%',
-    height: '335px',
+    width: '140px',
+    // maxWidth: '140px',
+    height: '140px',
+    // maxHeight: '140px',
   },
   priceText: {
     paddingTop: '10px',
     color: '#000',
     fontSize: '16px',
+  },
+  cardArea: {
+    display: ' flex',
+    flexDirection: 'row',
+    justifyContent: 'start',
+    alignItems: 'center',
+  },
+  cardAction: {
+    marginTop: '20px',
+    // display: ' flex',
+    // flexDirection: 'row',
+    // justifyContent: 'space-around',
+    // alignItems: 'center',
+  },
+  cartTotalBox: {
+    border: '1px solid #e6e6e6',
+  },
+  cartTotalInfo: {
+    borderTop: '1px solid #e6e6e6',
+    borderBottom: '1px solid #e6e6e6',
   },
 }))
 
@@ -41,14 +69,17 @@ const Cart = () => {
   return (
     <Container className={classes.container}>
       <Grid container spacing={3}>
-        <Grid item md={8}>
+        <Grid item xs={12} md={8}>
           <Card className={classes.root}>
-            <CardActionArea>
-              <CardMedia
-                className={classes.media}
-                image={product.image}
-                title={product.name}
-              />
+            <CardActionArea className={classes.cardArea}>
+              <Grid item xs={12}>
+                <CardMedia
+                  className={classes.media}
+                  image={product.image}
+                  title={product.name}
+                />
+              </Grid>
+
               <CardContent>
                 <Typography gutterBottom variant="h6" component="h2">
                   {product.name}
@@ -65,10 +96,155 @@ const Cart = () => {
                 </Typography>
               </CardContent>
             </CardActionArea>
+            <CardActions className={classes.cardAction}>
+              <Grid item xs={12}>
+                <ButtonGroup
+                  color="primary"
+                  aria-label="outlined primary button group"
+                >
+                  <IconButton aria-label="delete">
+                    <RemoveIcon />
+                  </IconButton>
+                  <TextField
+                    id="standard-number"
+                    label="Number"
+                    type="number"
+                    value={1}
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                  />
+                  <IconButton aria-label="delete">
+                    <AddIcon />
+                  </IconButton>
+                </ButtonGroup>
+              </Grid>
+              <Grid item xs={12}>
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  size="large"
+                  fullWidth
+                  className={classes.button}
+                  startIcon={<DeleteIcon />}
+                >
+                  Delete
+                </Button>
+              </Grid>
+              <Grid item xs={12}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  size="large"
+                  fullWidth
+                  className={classes.button}
+                  startIcon={<FavoriteBorderIcon />}
+                >
+                  Add To Favorites
+                </Button>
+              </Grid>
+            </CardActions>
           </Card>
         </Grid>
-        <Grid item md={4}>
-          bar
+        <Grid item xs={12} md={8}>
+          <Card className={classes.root}>
+            <CardActionArea className={classes.cardArea}>
+              <Grid item xs={12}>
+                <CardMedia
+                  className={classes.media}
+                  image={product.image}
+                  title={product.name}
+                />
+              </Grid>
+
+              <CardContent>
+                <Typography gutterBottom variant="h6" component="h2">
+                  {product.name}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  {product.description}
+                </Typography>
+                <Typography
+                  variant="h6"
+                  color="textSecondary"
+                  className={classes.priceText}
+                >
+                  ${product.price}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions className={classes.cardAction}>
+              <Grid item xs={12}>
+                <ButtonGroup
+                  color="primary"
+                  aria-label="outlined primary button group"
+                >
+                  <IconButton aria-label="delete">
+                    <RemoveIcon />
+                  </IconButton>
+                  <TextField
+                    id="standard-number"
+                    label="Number"
+                    type="number"
+                    value={1}
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                  />
+                  <IconButton aria-label="delete">
+                    <AddIcon />
+                  </IconButton>
+                </ButtonGroup>
+              </Grid>
+              <Grid item xs={12}>
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  size="large"
+                  fullWidth
+                  className={classes.button}
+                  startIcon={<DeleteIcon />}
+                >
+                  Delete
+                </Button>
+              </Grid>
+              <Grid item xs={12}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  size="large"
+                  fullWidth
+                  className={classes.button}
+                  startIcon={<FavoriteBorderIcon />}
+                >
+                  Add To Favorites
+                </Button>
+              </Grid>
+            </CardActions>
+          </Card>
+        </Grid>
+        <Grid item xs={12} md={4} className={classes.cartTotalBox}>
+          <h5 style={{ fontSize: '35px', margin: '10px' }}>Cart Totals</h5>
+          <div className={classes.cartTotalInfo}>
+            <p>
+              Subtotal: <span>$300</span>
+            </p>
+            <p>
+              Product Discounts: <span style={{ color: 'red' }}>$0</span>
+            </p>
+          </div>
+          <p style={{ fontSize: '25px' }}>
+            Total: <span>$300</span>
+          </p>
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            fullWidth
+            className={classes.button}
+          >
+            Proceed To Checkout
+          </Button>
         </Grid>
       </Grid>
     </Container>
