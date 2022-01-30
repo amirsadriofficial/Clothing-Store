4588bd6234609392d9bd039a1f7cf1445222bf73
Amir Sadri <amirsadriofficial@gmail.com>
2022-01-30 07:52:18 +0330

    Update

--- src/component/not-found/NotFound.jsx
+++ src/component/not-found/NotFound.jsx
@@ -1,7 +1,38 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Container from '@material-ui/core/Container'
+import Button from '@material-ui/core/Button'
+import { makeStyles } from '@material-ui/core'
+
+const useStyle = makeStyles({
+  container: {
+    height: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  numberStyle: {
+    fontSize: '200px',
+    margin: '0',
+  },
+  textStyle: {
+    fontSize: '45px',
+  },
+})
 
 const NotFound = () => {
-  return <p>Not Found Page</p>
+  const classes = useStyle()
+
+  return (
+    <Container className={classes.container}>
+      <p className={classes.numberStyle}>404</p>
+      <p className={classes.textStyle}>Page is Not Found</p>
+      <Button variant="outlined" color="primary">
+        <Link to="/">Back To Home</Link>
+      </Button>
+    </Container>
+  )
 }
 
 export default NotFound
