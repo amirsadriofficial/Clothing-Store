f3f5d8f48a90cc605599f5d147f7de5f7bc5070c
Amir Sadri <amirsadriofficial@gmail.com>
2022-02-02 13:55:37 +0330

    Update

--- src/redux/cart/Reducer.jsx
+++ src/redux/cart/Reducer.jsx
@@ -1,20 +1,20 @@
-import Types from './Types'
+// import Types from './Types'
 
-const cartReducer = (state, action) => {
-  switch (action.type) {
-    case Types.ADD_TO_CART:
-      return {
-        ...state,
-        cartList: state.cartList + 1,
-      }
-    case Types.REMOVE_FROM_CART:
-      return {
-        ...state,
-        cartList: state.cartList + 1,
-      }
-    default:
-      return state
-  }
-}
+// const cartReducer = (state, action) => {
+//   switch (action.type) {
+//     case Types.ADD_TO_CART:
+//       return {
+//         ...state,
+//         cartList: state.cartList + 1,
+//       }
+//     case Types.REMOVE_FROM_CART:
+//       return {
+//         ...state,
+//         cartList: state.cartList + 1,
+//       }
+//     default:
+//       return state
+//   }
+// }
 
-export default cartReducer
+// export default cartReducer
--- src/redux/favorites/Reducer.jsx
+++ src/redux/favorites/Reducer.jsx
@@ -1,20 +1,20 @@
-import Types from './Types'
+// import Types from './Types'
 
-const favoritesReducer = (state, action) => {
-  switch (action.type) {
-    case Types.ADD_TO_FAVORITES:
-      return {
-        ...state,
-        favoritesList: state.favoritesList + 1,
-      }
-    case Types.REMOVE_FROM_FAVORITES:
-      return {
-        ...state,
-        favoritesList: state.favoritesList + 1,
-      }
-    default:
-      return state
-  }
-}
+// const favoritesReducer = (state, action) => {
+//   switch (action.type) {
+//     case Types.ADD_TO_FAVORITES:
+//       return {
+//         ...state,
+//         favoritesList: state.favoritesList + 1,
+//       }
+//     case Types.REMOVE_FROM_FAVORITES:
+//       return {
+//         ...state,
+//         favoritesList: state.favoritesList + 1,
+//       }
+//     default:
+//       return state
+//   }
+// }
 
-export default favoritesReducer
+// export default favoritesReducer
