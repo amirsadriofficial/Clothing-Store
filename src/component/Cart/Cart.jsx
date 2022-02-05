37caa0dbd8c18025fce8cefffef3d40db25f9ddf
Amir Sadri <amirsadriofficial@gmail.com>
2022-02-05 09:30:12 +0330

    Update

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import Container from '@material-ui/core/Container'
 import Grid from '@material-ui/core/Grid'
@@ -16,6 +17,7 @@ import AddIcon from '@material-ui/icons/Add'
 import RemoveIcon from '@material-ui/icons/Remove'
 import DeleteIcon from '@material-ui/icons/Delete'
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder'
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart'
 
 const useStyles = makeStyles(() => ({
   container: {
@@ -62,13 +64,19 @@ const useStyles = makeStyles(() => ({
   pageTitle: {
     borderBottom: '1px solid #515151;',
   },
+  EmptySection: {
+    height: '70vh',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 }))
 
 const Cart = () => {
   const classes = useStyles()
   const carts = useSelector((state) => state.cartReducer)
-  // const product = Product.find((item) => item.id === 'bag_product_2')
-  console.log('carts:', carts)
+  // console.log('carts:', carts)
 
   return (
     <Container className={classes.container}>
@@ -76,209 +84,137 @@ const Cart = () => {
         <Grid item xs={12} className={classes.pageTitle}>
           <h2 className={classes.pageTitleText}>Cart</h2>
         </Grid>
-        {carts ? (
-          <Grid item xs={12} md={7}>
-            {carts.map((product) => (
-              <Grid
-                item
-                xs={12}
-                style={{ marginBottom: '10px' }}
-                key={product.id}
-              >
-                <Card className={classes.root}>
-                  <CardActionArea className={classes.cardArea}>
-                    <Grid item xs={5} sm={3}>
-                      <CardMedia
-                        className={classes.media}
-                        image={product.image}
-                        title={product.name}
-                      />
-                    </Grid>
-                    <Grid item xs={12}>
-                      <CardContent>
-                        <Typography
-                          gutterBottom
-                          variant="h6"
-                          component="h2"
-                          style={{ fontSize: '24px' }}
-                        >
-                          {product.name}
-                        </Typography>
-                        <Typography
-                          variant="body2"
-                          color="textSecondary"
-                          component="p"
-                        >
-                          {product.description}
-                        </Typography>
-                        <Typography
-                          variant="h6"
-                          color="textSecondary"
-                          className={classes.priceText}
-                        >
-                          ${product.price}
-                        </Typography>
-                      </CardContent>
-                    </Grid>
-                  </CardActionArea>
-                  <Grid container spacing={3} className={classes.cardAction}>
-                    <Grid item xs={8} sm={4} lg={4}>
-                      <ButtonGroup
-                        color="primary"
-                        aria-label="outlined primary button group"
-                      >
-                        <IconButton aria-label="delete">
-                          <RemoveIcon />
-                        </IconButton>
-                        <TextField
-                          id="standard-number"
-                          label="Number"
-                          type="number"
-                          value={1}
-                          InputLabelProps={{
-                            shrink: true,
-                          }}
+        {carts.carts.length > 0 ? (
+          <>
+            <Grid item xs={12} md={7}>
+              {carts.carts.map((product) => (
+                <Grid
+                  item
+                  xs={12}
+                  style={{ marginBottom: '10px' }}
+                  key={product.id}
+                >
+                  <Card className={classes.root}>
+                    <CardActionArea className={classes.cardArea}>
+                      <Grid item xs={5} sm={3}>
+                        <CardMedia
+                          className={classes.media}
+                          image={product.image}
+                          title={product.name}
                         />
-                        <IconButton aria-label="delete">
-                          <AddIcon />
-                        </IconButton>
-                      </ButtonGroup>
+                      </Grid>
+                      <Grid item xs={12}>
+                        <CardContent>
+                          <Typography
+                            gutterBottom
+                            variant="h6"
+                            component="h2"
+                            style={{ fontSize: '24px' }}
+                          >
+                            {product.name}
+                          </Typography>
+                          <Typography
+                            variant="body2"
+                            color="textSecondary"
+                            component="p"
+                          >
+                            {product.description}
+                          </Typography>
+                          <Typography
+                            variant="h6"
+                            color="textSecondary"
+                            className={classes.priceText}
+                          >
+                            ${product.price}
+                          </Typography>
+                        </CardContent>
+                      </Grid>
+                    </CardActionArea>
+                    <Grid container spacing={3} className={classes.cardAction}>
+                      <Grid item xs={8} sm={4} lg={4}>
+                        <ButtonGroup
+                          color="primary"
+                          aria-label="outlined primary button group"
+                        >
+                          <IconButton aria-label="delete">
+                            <RemoveIcon />
+                          </IconButton>
+                          <TextField
+                            id="standard-number"
+                            label="Number"
+                            type="number"
+                            value={1}
+                            InputLabelProps={{
+                              shrink: true,
+                            }}
+                          />
+                          <IconButton aria-label="delete">
+                            <AddIcon />
+                          </IconButton>
+                        </ButtonGroup>
+                      </Grid>
+                      <Grid item xs={4} sm={3} lg={2}>
+                        <Button
+                          variant="contained"
+                          color="secondary"
+                          fullWidth
+                          startIcon={<DeleteIcon />}
+                        >
+                          Delete
+                        </Button>
+                      </Grid>
+                      <Grid item xs={12} sm={5} lg={4}>
+                        <Button
+                          variant="contained"
+                          color="primary"
+                          fullWidth
+                          startIcon={<FavoriteBorderIcon />}
+                        >
+                          Add To Favorites
+                        </Button>
+                      </Grid>
                     </Grid>
-                    <Grid item xs={4} sm={3} lg={2}>
-                      <Button
-                        variant="contained"
-                        color="secondary"
-                        fullWidth
-                        startIcon={<DeleteIcon />}
-                      >
-                        Delete
-                      </Button>
-                    </Grid>
-                    <Grid item xs={12} sm={5} lg={4}>
-                      <Button
-                        variant="contained"
-                        color="primary"
-                        fullWidth
-                        startIcon={<FavoriteBorderIcon />}
-                      >
-                        Add To Favorites
-                      </Button>
-                    </Grid>
-                  </Grid>
-                </Card>
-              </Grid>
-            ))}
-            cart is full but dont work
-          </Grid>
-        ) : (
-          'Cart is Empty'
-        )}
-        {/* <Grid item xs={12} style={{ marginBottom: '10px' }}>
-          <Card className={classes.root}>
-            <CardActionArea className={classes.cardArea}>
-              <Grid item xs={5} sm={3}>
-                <CardMedia
-                  className={classes.media}
-                  image={product.image}
-                  title={product.name}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <CardContent>
-                  <Typography
-                    gutterBottom
-                    variant="h6"
-                    component="h2"
-                    style={{ fontSize: '24px' }}
-                  >
-                    {product.name}
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                  >
-                    {product.description}
-                  </Typography>
-                  <Typography
-                    variant="h6"
-                    color="textSecondary"
-                    className={classes.priceText}
-                  >
-                    ${product.price}
-                  </Typography>
-                </CardContent>
-              </Grid>
-            </CardActionArea>
-            <Grid container spacing={3} className={classes.cardAction}>
-              <Grid item xs={8} sm={4} lg={4}>
-                <ButtonGroup
-                  color="primary"
-                  aria-label="outlined primary button group"
-                >
-                  <IconButton aria-label="delete">
-                    <RemoveIcon />
-                  </IconButton>
-                  <TextField
-                    id="standard-number"
-                    label="Number"
-                    type="number"
-                    value={1}
-                    InputLabelProps={{
-                      shrink: true,
-                    }}
-                  />
-                  <IconButton aria-label="delete">
-                    <AddIcon />
-                  </IconButton>
-                </ButtonGroup>
-              </Grid>
-              <Grid item xs={4} sm={3} lg={2}>
-                <Button
-                  variant="contained"
-                  color="secondary"
-                  fullWidth
-                  startIcon={<DeleteIcon />}
-                >
-                  Delete
-                </Button>
-              </Grid>
-              <Grid item xs={12} sm={5} lg={4}>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  fullWidth
-                  startIcon={<FavoriteBorderIcon />}
-                >
-                  Add To Favorites
-                </Button>
-              </Grid>
+                  </Card>
+                </Grid>
+              ))}
             </Grid>
-          </Card>
-        </Grid> */}
-        <Grid item xs={12} sm={6} md={5} className={classes.cartTotalBox}>
-          <h5 style={{ fontSize: '35px', margin: '10px' }}>Cart Totals</h5>
-          <div className={classes.cartTotalInfo}>
-            <p>
-              Subtotal: <span>$300</span>
-            </p>
-            <p>
-              Product Discounts: <span style={{ color: 'red' }}>$0</span>
-            </p>
-          </div>
-          <p style={{ fontSize: '25px' }}>
-            Total: <span>$300</span>
-          </p>
-          <Button
-            variant="contained"
-            color="primary"
-            size="large"
-            fullWidth
-            className={classes.button}
-          >
-            Proceed To Checkout
-          </Button>
-        </Grid>
+            <Grid item xs={12} sm={6} md={5} className={classes.cartTotalBox}>
+              <h5 style={{ fontSize: '35px', margin: '10px' }}>Cart Totals</h5>
+              <div className={classes.cartTotalInfo}>
+                <p>
+                  Subtotal: <span>$300</span>
+                </p>
+                <p>
+                  Product Discounts: <span style={{ color: 'red' }}>$0</span>
+                </p>
+              </div>
+              <p style={{ fontSize: '25px' }}>
+                Total: <span>$300</span>
+              </p>
+              <Button
+                variant="contained"
+                color="primary"
+                size="large"
+                fullWidth
+                className={classes.button}
+              >
+                Proceed To Checkout
+              </Button>
+            </Grid>
+          </>
+        ) : (
+          <Grid item xs={12} className={classes.EmptySection}>
+            <AddShoppingCartIcon
+              style={{ fontSize: '200px', color: 'green' }}
+            />
+            <p style={{ fontSize: '25px', marginTop: '50px' }}>Cart is Empty</p>
+            <Link to="/shop">
+              <Button variant="outlined" color="primary" size="large" fullWidth>
+                Go Shopping
+              </Button>
+            </Link>
+          </Grid>
+        )}
       </Grid>
     </Container>
   )
--- src/container/Header/NavBar.jsx
+++ src/container/Header/NavBar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-// import { useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 import AppBar from '@material-ui/core/AppBar'
 import Box from '@material-ui/core/Box'
 import Toolbar from '@material-ui/core/Toolbar'
@@ -87,8 +87,7 @@ const pages = [
 
 const NavBar = () => {
   const classes = useStyles()
-  // const carts = useSelector((state) => state.cartReducer)
-  // const shoppingIconNumber = carts.length
+  const carts = useSelector((state) => state.cartReducer)
   const [anchorElNav, setAnchorElNav] = useState(null)
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget)
@@ -197,7 +196,7 @@ const NavBar = () => {
           </Box>
           <Link to="/cart">
             <IconButton aria-label="delete">
-              <Badge badgeContent={1} color="primary">
+              <Badge badgeContent={carts.carts.length} color="primary">
                 <ShoppingCartIcon />
               </Badge>
             </IconButton>
