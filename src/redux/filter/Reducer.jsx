35cd259f77ef62e0d0d02dfa0669f0fddb58f384
Amir Sadri <amirsadriofficial@gmail.com>
2022-02-08 22:01:13 +0330

    Update

--- src/redux/filter/Reducer.jsx
+++ src/redux/filter/Reducer.jsx
@@ -4,12 +4,10 @@ const filterReducer = (state = { filter: '' }, action) => {
   switch (action.type) {
     case Types.UP_TO_ONE_HUNDRED_PRICE:
       return {
-        ...state,
         filter: action.price,
       }
     case Types.ABOVE_ONE_HUNDRED_PRICE: {
       return {
-        ...state,
         filter: action.price,
       }
     }
