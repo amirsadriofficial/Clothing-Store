09bfd41d74542cdd6e2b5d02ba4011aceb6d5ecf
Amir Sadri <amirsadriofficial@gmail.com>
2022-02-08 21:40:05 +0330

    Update

--- src/redux/filter/Types.jsx
+++ src/redux/filter/Types.jsx
@@ -1,7 +1,6 @@
 const Types = {
   UP_TO_ONE_HUNDRED_PRICE: 'UP_TO_ONE_HUNDRED_PRICE',
   ABOVE_ONE_HUNDRED_PRICE: 'ABOVE_ONE_HUNDRED_PRICE',
-  // ALL_PRICE: 'ALL-PRICE',
 }
 
 export default Types
