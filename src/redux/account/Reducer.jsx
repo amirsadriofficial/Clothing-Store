2bc7d6fe67113a8bc589c63b22c4bb4ac31d11c9
Amir Sadri <amirsadriofficial@gmail.com>
2022-02-13 21:18:31 +0330

    Update

--- src/redux/account/Reducer.jsx
+++ src/redux/account/Reducer.jsx
@@ -1,6 +1,9 @@
 import Types from './Types'
 
 const accountReducer = (state = [], action) => {
+  let signinLogged = null
+  let signupLogged = null
+  let signoutLogged = null
   switch (action.type) {
     // Sign In
     case Types.SIGN_IN_STARTED:
@@ -10,6 +13,8 @@ const accountReducer = (state = [], action) => {
         loading: action.loading,
       }
     case Types.SIGN_IN_SUCCESS:
+      signinLogged = action.logged
+      localStorage.setItem('Logged', JSON.stringify(signinLogged))
       return {
         ...state,
         firstName: action.fName,
@@ -34,12 +39,14 @@ const accountReducer = (state = [], action) => {
         loading: action.loading,
       }
     case Types.SIGN_UP_SUCCESS:
+      signupLogged = action.logged
+      localStorage.setItem('Logged', JSON.stringify(signupLogged))
       return {
         ...state,
         firstName: action.fName,
         lastName: action.lName,
         email: action.email,
-        logged: action.logged,
+        logged: signupLogged,
         loading: action.loading,
         response: action.response,
       }
@@ -58,9 +65,11 @@ const accountReducer = (state = [], action) => {
         loading: action.loading,
       }
     case Types.SIGN_OUT_SUCCESS:
+      signoutLogged = action.logged
+      localStorage.setItem('Logged', JSON.stringify(signoutLogged))
       return {
         ...state,
-        logged: action.logged,
+        logged: signoutLogged,
         loading: action.loading,
         response: action.response,
       }
