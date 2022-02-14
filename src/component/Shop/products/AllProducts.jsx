f7e78b1c42c3f1fc5bd38137862bbce2ef13c93c
Amir Sadri <amirsadriofficial@gmail.com>
2022-02-14 22:27:50 +0330

    Update

--- src/component/Shop/products/AllProducts.jsx
+++ src/component/Shop/products/AllProducts.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { makeStyles } from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid'
@@ -27,50 +27,64 @@ const useStyles = makeStyles({
 
 const AllProducts = (filter) => {
   const classes = useStyles()
+  const [filteredProducts, setFilteredProducts] = useState(Products)
   console.log('filter:', filter)
+  // const handleFilter = () => {
+  if (filter === 'All Price') {
+    setFilteredProducts(Products)
+  }
+  const UpTo100 = Products.filter((product) => product.price < 100)
+  if (filter === 'Up To One Hundred Price') {
+    setFilteredProducts(UpTo100)
+  }
+  const Above100 = Products.filter((product) => product.price > 100)
+  if (filter === 'Above One Hundred Price') {
+    setFilteredProducts(Above100)
+  }
+  // }
 
   return (
     <Grid container spacing={3}>
-      {filter ? (
-        <>
-          {Products.map((product) => (
-            <Grid item xs={12} sm={6} md={4} lg={3}>
-              <Link to={`/product/${product.id}`}>
-                <Card className={classes.root}>
-                  <CardActionArea>
-                    <CardMedia
-                      className={classes.media}
-                      image={product.image}
-                      title={product.name}
-                    />
-                    <CardContent>
-                      <Typography gutterBottom variant="h6" component="h2">
-                        {product.name}
-                      </Typography>
-                      <Typography
-                        variant="body2"
-                        color="textSecondary"
-                        component="p"
-                      >
-                        {product.description}
-                      </Typography>
-                      <Typography
-                        variant="h6"
-                        color="textSecondary"
-                        className={classes.priceText}
-                      >
-                        ${product.price}
-                      </Typography>
-                    </CardContent>
-                  </CardActionArea>
-                </Card>
-              </Link>
-            </Grid>
-          ))}
-        </>
+      {/* {filter ? (
+        <> */}
+      {filteredProducts.map((product) => (
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <Link to={`/product/${product.id}`}>
+            <Card className={classes.root}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image={product.image}
+                  title={product.name}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h6" component="h2">
+                    {product.name}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    {product.description}
+                  </Typography>
+                  <Typography
+                    variant="h6"
+                    color="textSecondary"
+                    className={classes.priceText}
+                  >
+                    ${product.price}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Link>
+        </Grid>
+      ))}
+      {/* </>
       ) : (
         'All is not valid'
-      )}
+      )} */}
       {/* {filter ? (
         <>
           {Products.map((product) => (
