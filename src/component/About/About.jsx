9e2e6dbd0b1cad9da703d5e49cf6082b92b2d69d
Amir Sadri <amirsadriofficial@gmail.com>
2022-02-16 19:35:23 +0330

    Update

--- src/component/About/About.jsx
+++ src/component/About/About.jsx
@@ -2,74 +2,9 @@ import React from 'react'
 import Container from '@material-ui/core/Container'
 import Grid from '@material-ui/core/Grid'
 import Hidden from '@material-ui/core/Hidden'
-import { makeStyles } from '@material-ui/core/styles'
-import Background from '../../assets/images/background/about-bg.jpg'
+import useStyles from './Styles'
 import Gallery1 from '../../assets/images/gallery/about-01.jpg'
 import Gallery2 from '../../assets/images/gallery/about-02.jpg'
-
-const useStyles = makeStyles({
-  background: {
-    width: '100%',
-    height: '240px',
-    backgroundImage: `url(${Background})`,
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: '50px',
-    color: '#fff',
-  },
-  container: {
-    marginBottom: '75px',
-    marginTop: '75px',
-  },
-  imgSection: {
-    width: '100%',
-    maxWidth: '625px',
-    maxHeight: '625px',
-    position: 'relative',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  imgStyle: {
-    width: '90%',
-    height: '90%',
-    maxWidth: '500px',
-    maxHeight: '400px',
-    marginBottom: '100px',
-  },
-  backgroundImage: {
-    width: '90%',
-    height: '90%',
-    maxWidth: '500px',
-    maxHeight: '400px',
-    border: '3px solid #888',
-    position: 'absolute',
-    top: '25px',
-    right: '40px',
-    zIndex: '-1',
-  },
-  backgroundImageXs: {
-    width: '80%',
-    height: '80%',
-    maxWidth: '500px',
-    maxHeight: '400px',
-    border: '3px solid #666',
-    position: 'absolute',
-    top: '20px',
-    right: '15px',
-    zIndex: '-1',
-  },
-  titleSection: {
-    fontSize: '25px',
-  },
-  textSubSection: {
-    borderLeft: '3px solid #888',
-    paddingLeft: '20px',
-  },
-})
 
 const About = () => {
   const classes = useStyles()
