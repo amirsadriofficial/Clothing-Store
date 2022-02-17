ff8f0c90edaa0031ffd8d02191c2cfd1305a4de4
Amir Sadri <amirsadriofficial@gmail.com>
2022-02-17 14:16:20 +0330

    Update

---
+++ src/component/carousel/Carousel.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Carousel from 'react-multi-carousel'
+import 'react-multi-carousel/lib/styles.css'
+import Grid from '@material-ui/core/Grid'
+import Button from '@material-ui/core/Button'
+import Card from '../cart/Cart'
+import useStyles from './Styles'
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 4,
+    slidesToSlide: 4,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+    slidesToSlide: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+    slidesToSlide: 1,
+  },
+}
+
+const CarouselComponent = (Product) => {
+  const classes = useStyles()
+  const Products = Product
+
+  return (
+    <Grid className={classes.bodySection}>
+      <Grid item xs={6} sm={3} md={2} lg={2}>
+        <h3 className={classes.titleText}>Bag</h3>
+      </Grid>
+      <Carousel responsive={responsive} className={classes.carouselStyle}>
+        {Products.data.map((product) => (
+          <Card data={product} />
+        ))}
+      </Carousel>
+      <Grid item xs={12} className={classes.buttonStyle}>
+        <Grid item xs={9} sm={6} md={4} lg={3}>
+          <Link to="/shop">
+            <Button variant="contained" color="primary" fullWidth>
+              See All Products
+            </Button>
+          </Link>
+        </Grid>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default CarouselComponent
