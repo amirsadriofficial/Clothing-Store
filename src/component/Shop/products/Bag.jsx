fa8df1d769a2e2eca51889fa7c1d2f77c3368469
Amir Sadri <amirsadriofficial@gmail.com>
2022-02-17 17:05:16 +0330

    Update

--- src/component/Shop/products/Bag.jsx
+++ src/component/Shop/products/Bag.jsx
@@ -1,52 +1,13 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import Grid from '@material-ui/core/Grid'
-import Card from '@material-ui/core/Card'
-import CardActionArea from '@material-ui/core/CardActionArea'
-import CardContent from '@material-ui/core/CardContent'
-import CardMedia from '@material-ui/core/CardMedia'
-import Typography from '@material-ui/core/Typography'
+import Card from '../../card/Card'
 import Products from '../../../utils/bags-products'
-import useStyles from './Styles'
 
 const Bag = () => {
-  const classes = useStyles()
-
   return (
     <Grid container spacing={3}>
       {Products.map((product) => (
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-          <Link to={`/product/${product.id}`}>
-            <Card className={classes.root}>
-              <CardActionArea>
-                <CardMedia
-                  className={classes.media}
-                  image={product.image}
-                  title={product.name}
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h6" component="h2">
-                    {product.name}
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                  >
-                    {product.description}
-                  </Typography>
-                  <Typography
-                    variant="h6"
-                    color="textSecondary"
-                    className={classes.priceText}
-                  >
-                    ${product.price}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </Link>
-        </Grid>
+        <Card data={product} />
       ))}
     </Grid>
   )
