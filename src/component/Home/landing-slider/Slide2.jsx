a6990bcaccab39b0f4bcf1bc237f66569afdca5c
Amir Sadri <amirsadriofficial@gmail.com>
2022-02-17 17:29:09 +0330

    Update

--- src/component/Home/landing-slider/Slide2.jsx
+++ src/component/Home/landing-slider/Slide2.jsx
@@ -1,73 +1,14 @@
 import React from 'react'
-import Button from '@material-ui/core/Button'
-import Grid from '@material-ui/core/Grid'
-import Hidden from '@material-ui/core/Hidden'
 import SlideTwo from '../../../assets/images/slide/slide-02.jpg'
-import useStyles from './Styles'
+import Slide from '../../slide/Slide'
 
 const Slide2 = () => {
-  const classes = useStyles()
-
   return (
-    <>
-      <img src={SlideTwo} alt="SlideTwo" className={classes.responsive} />
-      <Hidden mdDown>
-        <Grid className={classes.descriptionPosition}>
-          <h2 className={classes.descriptionFont}>Man New-Season</h2>
-          <p className={classes.descriptionFont}>JACKETS & COATS</p>
-          <Button
-            className={classes.buttonStyle}
-            style={{ width: '200px', height: '50px', fontSize: '20px' }}
-            variant="contained"
-            color="primary"
-          >
-            Shop Now
-          </Button>
-        </Grid>
-      </Hidden>
-      <Hidden lgUp smDown>
-        <Grid className={classes.descriptionPositionLg}>
-          <h2 className={classes.descriptionFontLg}>Man New-Season</h2>
-          <p className={classes.descriptionFontLg}>JACKETS & COATS</p>
-          <Button
-            className={classes.buttonStyle}
-            size="large"
-            variant="contained"
-            color="primary"
-          >
-            Shop Now
-          </Button>
-        </Grid>
-      </Hidden>
-      <Hidden mdUp xsDown>
-        <Grid className={classes.descriptionPositionMd}>
-          <h2 className={classes.descriptionFontMd}>Man New-Season</h2>
-          <p className={classes.descriptionFontMd}>JACKETS & COATS</p>
-          <Button
-            className={classes.buttonStyle}
-            variant="contained"
-            color="primary"
-          >
-            Shop Now
-          </Button>
-        </Grid>
-      </Hidden>
-      <Hidden smUp>
-        <Grid className={classes.descriptionPositionSm}>
-          <h2 className={classes.descriptionFontSm}>Man New-Season</h2>
-          <p className={classes.descriptionFontSm}>JACKETS & COATS</p>
-          <Button
-            className={classes.buttonStyle}
-            style={{ fontSize: '8px' }}
-            size="small"
-            variant="contained"
-            color="primary"
-          >
-            Shop Now
-          </Button>
-        </Grid>
-      </Hidden>
-    </>
+    <Slide
+      image={SlideTwo}
+      title="Man New-Season"
+      description="JACKETS & COATS"
+    />
   )
 }
 
--- src/component/Home/landing-slider/Slide3.jsx
+++ src/component/Home/landing-slider/Slide3.jsx
@@ -1,73 +1,14 @@
 import React from 'react'
-import Button from '@material-ui/core/Button'
-import Grid from '@material-ui/core/Grid'
-import Hidden from '@material-ui/core/Hidden'
 import SlideThree from '../../../assets/images/slide/slide-03.jpg'
-import useStyles from './Styles'
+import Slide from '../../slide/Slide'
 
 const Slide3 = () => {
-  const classes = useStyles()
-
   return (
-    <>
-      <img src={SlideThree} alt="SlideThree" className={classes.responsive} />
-      <Hidden mdDown>
-        <Grid className={classes.descriptionPosition}>
-          <h2 className={classes.descriptionFont}>Man Collection 2022</h2>
-          <p className={classes.descriptionFont}>NEW ARRIVALS</p>
-          <Button
-            className={classes.buttonStyle}
-            style={{ width: '200px', height: '50px', fontSize: '20px' }}
-            variant="contained"
-            color="primary"
-          >
-            Shop Now
-          </Button>
-        </Grid>
-      </Hidden>
-      <Hidden lgUp smDown>
-        <Grid className={classes.descriptionPositionLg}>
-          <h2 className={classes.descriptionFontLg}>Man Collection 2022</h2>
-          <p className={classes.descriptionFontLg}>NEW ARRIVALS</p>
-          <Button
-            className={classes.buttonStyle}
-            size="large"
-            variant="contained"
-            color="primary"
-          >
-            Shop Now
-          </Button>
-        </Grid>
-      </Hidden>
-      <Hidden mdUp xsDown>
-        <Grid className={classes.descriptionPositionMd}>
-          <h2 className={classes.descriptionFontMd}>Man Collection 2022</h2>
-          <p className={classes.descriptionFontMd}>NEW ARRIVALS</p>
-          <Button
-            className={classes.buttonStyle}
-            variant="contained"
-            color="primary"
-          >
-            Shop Now
-          </Button>
-        </Grid>
-      </Hidden>
-      <Hidden smUp>
-        <Grid className={classes.descriptionPositionSm}>
-          <h2 className={classes.descriptionFontSm}>Man Collection 2022</h2>
-          <p className={classes.descriptionFontSm}>NEW ARRIVALS</p>
-          <Button
-            className={classes.buttonStyle}
-            style={{ fontSize: '8px' }}
-            size="small"
-            variant="contained"
-            color="primary"
-          >
-            Shop Now
-          </Button>
-        </Grid>
-      </Hidden>
-    </>
+    <Slide
+      image={SlideThree}
+      title="Man Collection 2022"
+      description="NEW ARRIVALS"
+    />
   )
 }
 
