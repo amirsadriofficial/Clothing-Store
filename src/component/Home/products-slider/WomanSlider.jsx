ed5e120b62c79cb94f6b3149025e45b97b676b1f
Amir Sadri <amirsadriofficial@gmail.com>
2022-02-17 14:25:41 +0330

    Update

--- src/component/Home/products-slider/WomanSlider.jsx
+++ src/component/Home/products-slider/WomanSlider.jsx
@@ -1,90 +1,9 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import Carousel from 'react-multi-carousel'
-import 'react-multi-carousel/lib/styles.css'
-import Grid from '@material-ui/core/Grid'
-import Card from '@material-ui/core/Card'
-import CardActionArea from '@material-ui/core/CardActionArea'
-import CardContent from '@material-ui/core/CardContent'
-import CardMedia from '@material-ui/core/CardMedia'
-import Typography from '@material-ui/core/Typography'
-import Button from '@material-ui/core/Button'
-import Products from '../../../utils/women-products'
-import useStyles from './Styles'
-
-const responsive = {
-  desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 4,
-    slidesToSlide: 4,
-  },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 2,
-    slidesToSlide: 2,
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1,
-    slidesToSlide: 1,
-  },
-}
+import Carousel from '../../carousel/Carousel'
+import Product from '../../../utils/women-products'
 
 const WomanSlider = () => {
-  const classes = useStyles()
-
-  return (
-    <Grid className={classes.bodySection}>
-      <Grid item xs={6} sm={3} md={2} lg={2}>
-        <h3 className={classes.titleText}>Woman</h3>
-      </Grid>
-      <Carousel responsive={responsive} className={classes.carouselStyle}>
-        {Products.map((product) => (
-          <Grid item xs={12} sm={6} md={4} lg={4}>
-            <Link to={`/product/${product.id}`}>
-              <Card className={classes.root}>
-                <CardActionArea>
-                  <CardMedia
-                    className={classes.media}
-                    image={product.image}
-                    title={product.name}
-                  />
-                  <CardContent>
-                    <Typography gutterBottom variant="h6" component="h2">
-                      {product.name}
-                    </Typography>
-                    <Typography
-                      variant="body2"
-                      color="textSecondary"
-                      component="p"
-                    >
-                      {product.description}
-                    </Typography>
-                    <Typography
-                      variant="h6"
-                      color="textSecondary"
-                      className={classes.priceText}
-                    >
-                      ${product.price}
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-              </Card>
-            </Link>
-          </Grid>
-        ))}
-      </Carousel>
-      <Grid item xs={12} className={classes.buttonStyle}>
-        <Grid item xs={9} sm={6} md={4} lg={3}>
-          <Link to="/shop">
-            <Button variant="contained" color="primary" fullWidth>
-              See All Products
-            </Button>
-          </Link>
-        </Grid>
-      </Grid>
-    </Grid>
-  )
+  return <Carousel data={Product} />
 }
 
 export default WomanSlider
