5f113cb90b2bab67ab6fceae3df464fd2ab9d002
Amir Sadri <amirsadriofficial@gmail.com>
2022-02-21 18:09:45 +0330

    Update

--- src/component/Shop/products/Man.jsx
+++ src/component/Shop/products/Man.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Grid from '@material-ui/core/Grid'
 import Card from '../../card/Card'
+import Placeholder from '../../placeholder/Placeholder'
 import Products from '../../../utils/men-products'
 
 const Man = () => {
+  const [placeholder, setPlaceholder] = useState(false)
+  const numOfPlaceholder = []
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < Products.length; i++) {
+    numOfPlaceholder.push(<Placeholder />)
+  }
+  setTimeout(() => {
+    setPlaceholder(true)
+  }, 1000)
+
   return (
     <Grid container spacing={3}>
-      {Products.map((product) => (
-        <Card data={product} />
-      ))}
+      {placeholder
+        ? Products.map((product) => <Card data={product} />)
+        : numOfPlaceholder.map((PlaceholderItems) => PlaceholderItems)}
     </Grid>
   )
 }
