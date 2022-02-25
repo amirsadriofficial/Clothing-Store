529864d681c034d5907e6db01c1e48779f91d58e
Amir Sadri <amirsadriofficial@gmail.com>
2022-02-25 13:41:08 +0330

    Update

--- src/component/favorites/Favorites.jsx
+++ src/component/favorites/Favorites.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import Container from '@material-ui/core/Container'
 import Grid from '@material-ui/core/Grid'
