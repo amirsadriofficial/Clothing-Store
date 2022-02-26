019a8f71ab2ea41be4212a752796835421f18cb7
Amir Sadri <amirsadriofficial@gmail.com>
2022-02-26 12:23:28 +0330

    Update

--- src/redux/cart/Action.jsx
+++ src/redux/cart/Action.jsx
@@ -4,6 +4,7 @@ export const ADD_TO_CART_ACTION = (product) => {
   return {
     type: Types.ADD_TO_CART,
     product,
+    qty: 1,
   }
 }
 
@@ -11,5 +12,6 @@ export const REMOVE_FROM_CART_ACTION = (product) => {
   return {
     type: Types.REMOVE_FROM_CART,
     product,
+    qty: 1,
   }
 }
