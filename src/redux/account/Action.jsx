a6502420383e8781b1259e5fab6dbc9c9884933a
Amir Sadri <amirsadriofficial@gmail.com>
2022-03-02 20:17:19 +0330

    Update

--- src/redux/account/Action.jsx
+++ src/redux/account/Action.jsx
@@ -1,13 +1,6 @@
 import Types from './Types'
 
 // Sign In
-const SIGN_IN_STARTED_ACTION = () => {
-  return {
-    type: Types.SIGN_IN_STARTED,
-    logged: false,
-    loading: true,
-  }
-}
 const SIGN_IN_SUCCESS_ACTION = (formValue) => {
   return {
     type: Types.SIGN_IN_SUCCESS,
