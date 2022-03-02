6b83ddabe32f5e660aa428642034cb8840740bfa
Amir Sadri <amirsadriofficial@gmail.com>
2022-03-02 20:18:05 +0330

    Update

--- src/redux/account/Action.jsx
+++ src/redux/account/Action.jsx
@@ -13,22 +13,7 @@ const SIGN_IN_SUCCESS_ACTION = (formValue) => {
     response: 'You have successfully signed in!',
   }
 }
-const SIGN_IN_FAILED_ACTION = () => {
-  return {
-    type: Types.SIGN_IN_FAILED,
-    logged: false,
-    loading: false,
-    response: 'Your request is invalid, please try again!',
-  }
-}
 // Sign Up
-const SIGN_UP_STARTED_ACTION = () => {
-  return {
-    type: Types.SIGN_UP_STARTED,
-    logged: false,
-    loading: true,
-  }
-}
 const SIGN_UP_SUCCESS_ACTION = (formValue) => {
   return {
     type: Types.SIGN_UP_SUCCESS,
@@ -41,36 +26,13 @@ const SIGN_UP_SUCCESS_ACTION = (formValue) => {
     response: 'You have successfully signed up!',
   }
 }
-const SIGN_UP_FAILED_ACTION = () => {
-  return {
-    type: Types.SIGN_UP_FAILED,
-    logged: false,
-    loading: false,
-    response: 'Your request is invalid, please try again!',
-  }
-}
 // Sign Out
-const SIGN_OUT_STARTED_ACTION = () => {
-  return {
-    type: Types.SIGN_OUT_STARTED,
-    logged: true,
-    loading: true,
-  }
-}
 const SIGN_OUT_SUCCESS_ACTION = () => {
   return {
     type: Types.SIGN_OUT_SUCCESS,
     logged: false,
     loading: false,
     response: 'You have successfully signed out!',
-  }
-}
-const SIGN_OUT_FAILED_ACTION = () => {
-  return {
-    type: Types.SIGN_OUT_FAILED,
-    logged: true,
-    loading: false,
-    response: 'Your request is invalid, please try again!',
   }
 }
 
