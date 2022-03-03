d33218f0049c3947a7056dfcda0fe11510e07609
Amir Sadri <amirsadriofficial@gmail.com>
2022-03-03 22:40:04 +0330

    Update

--- src/container/header/app-bar/IconItems.jsx
+++ src/container/header/app-bar/IconItems.jsx
@@ -12,8 +12,6 @@ const IconItems = () => {
   const classes = useStyles()
   const carts = useSelector((state) => state.cartReducer)
   const favorites = useSelector((state) => state.favoritesReducer)
-  // const carts = JSON.parse(localStorage.getItem('Carts'))
-  // const favorites = JSON.parse(localStorage.getItem('Favorites'))
 
   return (
     <>
